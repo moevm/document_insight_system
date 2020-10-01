e2a1b6ecea6c3a9caf99cacb3457b6e88ac88fa8
milestyoma <dornentertainment@gmail.com>
2020-10-02 00:16:50 +0300

    added html, js, css

--- app/server.py
+++ app/server.py
@@ -1,44 +1,60 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for
 import app.user.user as user
 import app.main.main as main
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="./../public/")
 
 
 # User pages request handlers:
 
 @app.route("/login")
 def login():
-    return user.login(request.args)
+    user.login(request.args)
+    return app.send_static_file("./views/login.html")
 
 
 @app.route("/signup")
 def signup():
-    return user.signup(request.args)
+    user.signup(request.args)
+    return app.send_static_file("./views/signup.html")
 
 
 # Main pages request handlers:
 
 @app.route("/upload")
 def upload():
-    return main.upload(request.args)
+    main.upload(request.args)
+    return app.send_static_file("./views/upload.html")
 
 
 @app.route("/results")
 def results():
-    return main.results(request.args)
+    main.results(request.args)
+    return app.send_static_file("./views/results.html")
 
 
 @app.route("/criteria")
 def criteria():
-    return main.criteria(request.args)
+    main.criteria(request.args)
+    return app.send_static_file("./views/criteria.html")
 
 
 # Redirection:
 
 @app.route("/")
 def default():
-    return "redirect to where?"
+    return redirect(url_for("login"))
+
+
+# Disable caching:
+
+@app.after_request
+def add_header(r):
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 
 if __name__ == '__main__':
