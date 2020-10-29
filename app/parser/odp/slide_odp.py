96d4c950da31a392310a9b43275e8130ccb8e875
milesartemius <shveitsar215@gmail.com>
2020-10-29 16:25:18 +0300

    Partly enumeration checkin

--- app/parser/odp/slide_odp.py
+++ app/parser/odp/slide_odp.py
@@ -8,15 +8,38 @@ def _is_title(node):
     return False
 
 
+def _is_notes(node):
+    return node.tagName == "presentation:notes"
+
+
+def _contains_page_number(node):
+    for key in node.attributes:
+        if key[1] == "page-number":
+            return True
+    return False
+
+
 class SlideODP(SlideBasic):
     def __init__(self, container):
         SlideBasic.__init__(self, container)
         title = []
         texts = []
 
+        self.page_number = [-1.0, -1.0, -1]
+
         for node in container.childNodes:
             if _is_title(node):
                 self.__walk_children(node, title)
+            elif _is_notes(node):
+                for child in node.childNodes:
+                    if _contains_page_number(child):
+                        for key in child.attributes:
+                            if key[1] == "page-number":
+                                self.page_number[2] = int(child.attributes[key])
+                            elif key[1] == "x":
+                                self.page_number[0] = float(child.attributes[key][:-3])
+                            elif key[1] == "y":
+                                self.page_number[1] = float(child.attributes[key][:-3])
             else:
                 node_text = []
                 self.__walk_children(node, node_text)
