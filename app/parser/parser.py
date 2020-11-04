8fc04b0d84357a5904054faac243d39e412dad72
milesartemius <shveitsar215@gmail.com>
2020-11-04 14:17:32 +0300

    Revert "removed unrelatid files"
    This reverts commit 82908f36b0d71753f0e3019ec2f679c2eaf6efce.

---
+++ app/parser/parser.py
@@ -0,0 +1,78 @@
+from app.parser.odp.presentation_odp import PresentationODP
+from app.parser.pptx.presentation_pptx import PresentationPPTX
+import re
+import os
+
+
+class Parser:
+    def __init__(self, presentation_name):
+        self.presentation_name = presentation_name
+        self.presentation = None
+        self.state = 0
+        self.text = []
+        self.titles = []
+        self.parse_presentation()
+
+    def get_presentation_name(self):
+        return self.presentation_name
+
+    def parse_presentation(self):
+        if str(self.presentation_name).endswith('.ppt') or str(self.presentation_name).endswith('.pptx'):
+            try:
+                self.state = 1
+                self.presentation = PresentationPPTX(self.presentation_name)
+                self.state = 2
+                self.text = self.presentation.get_text_from_slides()
+                self.titles = self.presentation.get_titles()
+                self.state = 3
+            except Exception as err:
+                print(err)
+                self.state = -1
+        elif str(self.presentation_name).endswith('.odp'):
+            try:
+                self.state = 1
+                self.presentation = PresentationODP(self.presentation_name)
+                self.state = 2
+                self.text = self.presentation.get_text_from_slides()
+                self.titles = self.presentation.get_titles()
+                self.state = 3
+            except Exception as err:
+                print(err)
+                self.state = -1
+
+    def get_titles(self):
+        return self.titles
+
+    def get_text(self):
+        return self.text
+
+    def get_state(self):
+        return self.state
+
+    def check_title_size(self, filename, upload_folder):
+        i = 0
+        error_slides = ''
+        for title in self.titles:
+            i += 1
+            title = str(title).replace('\x0b', '\n')
+            if '\n' in title or '\r' in title:
+                titles = []
+                for t in re.split('\r|\n', title):
+                    if t != '':
+                        titles.append(t)
+                if len(titles) > 2:
+                    error_slides += str(i) + ' '
+        try:
+            with open(upload_folder + '/' + os.path.splitext(filename)[0] + '_error_with_title_size.txt', 'w') as answer:
+                answer.write(error_slides)
+        except Exception as err:
+            print(err)
+            print("Что-то пошло не так")
+        return error_slides
+
+    def find_definite_slide(self, type_of_slide):
+        i = 0
+        for title in self.titles:
+            i += 1
+            if str(title).lower().find(str(type_of_slide).lower()) != -1:
+                return i
---
+++ app/server.py
@@ -0,0 +1,72 @@
+from flask import Flask, request, redirect, url_for, render_template
+import app.user.user as user
+import app.main.main as main
+
+
+ALLOWED_EXTENSIONS = {'pptx', 'odp', 'ppt'}
+UPLOAD_FOLDER = '../files'
+
+app = Flask(__name__, static_folder="./../public/", template_folder="./../templates/")
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+# User pages request handlers:
+
+@app.route("/login", methods=["GET"])
+def login():
+    user.login(request.args)
+    return render_template("./login.html", navi_upload=True, logout=False, name=(" ", " "))
+
+
+@app.route("/signup", methods=["GET"])
+def signup():
+    user.signup(request.args)
+    return render_template("./signup.html", navi_upload=True, logout=False, name=(" ", " "))
+
+
+# Main pages request handlers:
+
+@app.route("/upload", methods=["GET", "POST"])
+def upload():
+    if request.method == "POST":
+        return str(main.upload(request, UPLOAD_FOLDER))
+    elif request.method == "GET":
+        return render_template("./upload.html", debug=True, navi_upload=False, logout=True, name=("Имя", "Фамилия"))
+
+
+@app.route("/results", methods=["GET"])
+def results():
+    result = main.results(request.args)
+    return render_template("./results.html", navi_upload=True, logout=True, name=("Имя", "Фамилия"), results=result)
+
+
+@app.route("/criteria", methods=["GET"])
+def criteria():
+    return render_template("./criteria.html", navi_upload=True, logout=True, name=("Имя", "Фамилия"))
+
+
+@app.route("/status", methods=["GET"])
+def status():
+    return str(main.status())
+
+
+# Redirection:
+
+@app.route("/")
+def default():
+    return redirect(url_for("login"))
+
+
+# Disable caching:
+
+@app.after_request
+def add_header(r):
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
