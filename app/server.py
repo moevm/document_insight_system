afeab9fb6f6785dc361215859d43621cccd7f565
milesartemius <shveitsar215@gmail.com>
2020-12-01 00:57:33 +0300

    debug headers added

--- app/server.py
+++ app/server.py
@@ -141,10 +141,11 @@ def default():
 
 @app.after_request
 def add_header(r):
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    r.headers['Cache-Control'] = 'public, max-age=0'
+    if DEBUG:
+        r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        r.headers["Pragma"] = "no-cache"
+        r.headers["Expires"] = "0"
+        r.headers['Cache-Control'] = 'public, max-age=0'
     return r
 
 
