aaa90218ca82c8adc048d11934eb9412dee99f5a
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2021-06-15 17:20:18 +0300

    fixed db dir name

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -4,7 +4,7 @@ apache_config_filename=${1}
 apache_ssl_mod=${2:-''}
 sudo scripts/setup_apache_config.sh $apache_config_filename $apache_ssl_mod
 
-mkdir -p ../mongo_data
+mkdir -p ../slides_checker_mongo_data
 docker-compose down --rmi local
 docker-compose build
 docker-compose up -d --remove-orphans
