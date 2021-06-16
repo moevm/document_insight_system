bfa16b1bbc397a60828d7ef2e62950960e1ca988
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2021-06-16 17:20:48 +0300

    added build no cache

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -6,5 +6,5 @@ sudo scripts/setup_apache_config.sh $apache_config_filename $apache_ssl_mod
 
 mkdir -p ../slides_checker_mongo_data
 docker-compose down --rmi local
-docker-compose build
+docker-compose build --no-cache
 docker-compose up -d --remove-orphans
