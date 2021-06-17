2e6745406069d856e6a849fe3375cbdcc06852d0
John C. Alastor <42917151+kinekt2000@users.noreply.github.com>
2021-06-17 16:44:54 +0300

    conditional decorator
    декоратор для установки условия на другие декораторы

---
+++ app/utils/decorators.py
@@ -0,0 +1,8 @@
+def condition(decorator, condition):
+    if not condition:
+        def inner(func):
+            def wrapper(*args, **kwargs):
+                return func(*args, **kwargs)
+            return wrapper
+        return inner
+    return decorator
