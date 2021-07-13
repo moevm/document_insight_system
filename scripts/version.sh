53949e8769760fd03e6425edfe9e6e34c68b836a
Dmitry Ivanov <ivanovd.v3.0@gmail.com>
2021-07-13 13:07:03 +0300

    upd get branch for version

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,5 +1,5 @@
 COMMIT=$(git rev-parse HEAD)
-BRANCH=$(git status |head -1| cut -d' ' -f 3)
+BRANCH=$(git symbolic-ref HEAD | sed 's!refs\/heads\/!!')
 DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 VERSION=$(git tag | tail -1)
 # VERSION_FILE_PATH="$(dirname $(dirname $(readlink -f $0)))/$VERSION_FILE_NAME"
