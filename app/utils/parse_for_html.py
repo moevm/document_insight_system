f4128cc8bcb412f7e6cb5d51d62c2c34a605a366
Sasha Kogan <postmoderndeath@gmail.com>
2021-08-14 09:38:38 +0300

    rf parse for html

--- app/utils/parse_for_html.py
+++ app/utils/parse_for_html.py
@@ -1,11 +1,19 @@
-def format_descriptions(desc_list):
-    return ['<div class="format_description">' + item + '<br></div>' for item in desc_list]
+tags = {'br': '<br>', 'close_div': '</div>', 'div_class': '<div class="format_description">'}
+
+def format_descriptions(desc_list, open_tag = '', close_tag = ''):
+    return list(map(lambda item: open_tag + item + close_tag, desc_list))
+
+def format_header(header, close_tag = ''):
+    return header + close_tag
 
 def find_tasks_on_slides_feedback(slides_with_tasks):
-    return 'Всего задач: {}'.format(slides_with_tasks.get('count')) + '<br>', \
-           'Распознанные задачи: ', *format_descriptions(slides_with_tasks.get('recognized')), \
-           'Не найдены: ', *format_descriptions(slides_with_tasks.get('not_found'))
+    return format_header('Всего задач: {}'.format(slides_with_tasks.get('count')), tags.get('br')), \
+           'Распознанные задачи: ', \
+           *format_descriptions(slides_with_tasks.get('recognized'), tags.get('div_class'), tags.get('br') + tags.get('close_div')), \
+           'Не найдены: ', \
+           *format_descriptions(slides_with_tasks.get('not_found'), tags.get('div_class'), tags.get('br') + tags.get('close_div'))
 
 def tasks_conclusions_feedback(results):
-    return 'Соответствует на {}%'.format(results[0]) + '<br>', \
-           'Распознанные заключения: ', *format_descriptions(results[2])
+    return format_header('Соответствует на {}%'.format(results[0]), tags.get('br')), \
+           'Распознанные заключения: ', \
+            *format_descriptions(results[2], tags.get('div_class'), tags.get('br') + tags.get('close_div'))
