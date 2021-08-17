9ea0ba4acd44e639a228063e25c1f63cd4ee82b4
Sasha Kogan <postmoderndeath@gmail.com>
2021-08-17 06:41:11 +0300

    pass secret

--- app/lti_session_passback/lti/check_request.py
+++ app/lti_session_passback/lti/check_request.py
@@ -12,7 +12,9 @@ def check_request(request):
 
 
 def _check_request(request):
-    provider = FlaskToolProvider.from_flask_request(request = request)
+    provider = FlaskToolProvider.from_flask_request(
+              secret = ConsumersDBManager.get_secret(request.args.get('oauth_consumer_key', None)),
+              request = request)
     return provider.is_valid_request(LTIRequestValidator())
 
 
