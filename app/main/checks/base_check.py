b9a3b1613c99736257dacebe554ab45717f2f5b8
Sasha Kogan <postmoderndeath@gmail.com>
2021-09-20 11:24:11 +0300

    partial page linking support

--- app/main/checks/base_check.py
+++ app/main/checks/base_check.py
@@ -1,3 +1,5 @@
+from flask import url_for
+
 def answer(mod, value, *args):
     return {
         'pass': bool(mod),
@@ -11,3 +13,11 @@ class BaseCheck:
 
     def check(self):
         raise NotImplementedError()
+
+    def format_page_link(self, error):
+        base_pdf_link = url_for('get_pdf', _id=self.pdf_id)
+        res = []
+        for err in error:
+            page = base_pdf_link + "#page=" + str(err)
+            res.append('<a href='+ '"' + page + '"'+ 'target="_blank" rel="noopener">' + str(err) + '<a>')
+        return res
