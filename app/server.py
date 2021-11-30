575b56842164af9e5ae87995c0dedcd3940fce46
Sasha Kogan <postmoderndeath@gmail.com>
2021-11-30 13:41:20 +0300

    to do: mv reused filter pasre

--- app/server.py
+++ app/server.py
@@ -1,14 +1,14 @@
 from datetime import datetime, timedelta
 import logging
 from sys import argv
-
+import pandas as pd
 import json
+import csv
 import bson
 import pymongo
 from bson import ObjectId
 from flask import Flask, request, redirect, url_for, render_template, Response, abort, jsonify
 from flask_login import LoginManager, login_user, current_user, login_required, logout_user
-
 
 import app.servants.user as user
 from app.servants import data as data
@@ -301,6 +301,101 @@ def check_list_data():
     # return json data
     return jsonify(response)
 
+@app.route("/get_csv", methods=["GET", "POST"])
+@login_required
+def get_csv():
+    filters = request.args.get("filter", "{}")
+    try:
+        filters = json.loads(filters)
+        filters = filters if filters else {}
+    except Exception as e:
+        logger.warning("Can't parse filters")
+        logger.warning(repr(e))
+        filters = {}
+
+    filter_query = {}
+    if f_filename := filters.get("filename", None):
+        filter_query["filename"] = { "$regex": f_filename }
+
+    if f_user := filters.get("user", None):
+        filter_query["user"] = { "$regex": f_user }
+
+    f_upload_date = filters.get("upload-date", "")
+    f_upload_date_list = list(filter(lambda val: val, f_upload_date.split("-")))
+    try:
+        if len(f_upload_date_list) == 1:
+            date = datetime.strptime(f_upload_date_list[0], "%d.%m.%Y")
+            filter_query["_id"] = {
+                "$gte": ObjectId.from_datetime(date),
+                "$lte": ObjectId.from_datetime(date + timedelta(hours=23, minutes=59, seconds=59))
+            }
+        elif len(f_upload_date_list) > 1:
+            filter_query["_id"] = {
+                "$gte": ObjectId.from_datetime(datetime.strptime(f_upload_date_list[0], "%d.%m.%Y")),
+                "$lte": ObjectId.from_datetime(datetime.strptime(f_upload_date_list[1], "%d.%m.%Y"))
+            }
+    except Exception as e:
+        logger.warning("Can't apply upload-date filter")
+        logger.warning(repr(e))
+
+    f_moodle_date = filters.get("moodle-date", "")
+    f_moodle_date_list = list(filter(lambda val: val, f_moodle_date.split("-")))
+    try:
+        if len(f_moodle_date_list) == 1:
+            date = datetime.strptime(f_moodle_date_list[0], "%d.%m.%Y")
+            filter_query['lms_passback_time'] = {
+                "$gte": date,
+                "$lte": date + timedelta(hours=23, minutes=59, seconds=59)
+            }
+        elif len(f_moodle_date_list) > 1:
+            filter_query['lms_passback_time'] = {
+                "$gte": datetime.strptime(f_moodle_date_list[0], "%d.%m.%Y"),
+                "$lte": datetime.strptime(f_moodle_date_list[1], "%d.%m.%Y")
+            }
+    except Exception as e:
+        logger.warning("Can't apply moodle-date filter")
+        logger.warning(repr(e))
+
+    f_score = filters.get("score", "")
+    f_score_list = list(filter(lambda val: val, f_score.split("-")))
+    try:
+        if len(f_score_list) == 1:
+            filter_query["score"] = float(f_score_list[0])
+        elif len(f_score_list) > 1:
+            filter_query["score"] = {
+                "$gte": float(f_score_list[0]),
+                "$lte": float(f_score_list[1])
+            }
+    except Exception as e:
+        logger.warning("Can't apply score filter")
+        logger.warning(repr(e))
+
+    if not current_user.is_admin:
+        filter_query["user"] = current_user.username
+
+    limit = False
+    offset = False
+    sort = request.args.get("sort", "")
+    sort = 'upload-date' if not sort else sort
+    order = request.args.get("order", "")
+    order = 'desc' if not order else order
+    sort = "_id" if sort == "upload-date" else sort
+
+    rows, count = bd_helper.get_checks_cursor(filter=filter_query, limit=limit, offset=offset, sort=sort, order=order)
+    response = [{
+            "_id": str(item["_id"]),
+            "filename": item["filename"],
+            "user": item["user"],
+            "upload-date": item["_id"].generation_time.strftime("%d.%m.%Y %H:%M:%S"),
+            "moodle-date": item['lms_passback_time'].strftime("%d.%m.%Y %H:%M:%S") if item.get('lms_passback_time') else '-',
+            "score": item["score"]
+        } for item in rows]
+        
+    df = pd.read_json(json.dumps(response))
+    return Response(
+        df.to_csv(),
+        mimetype="text/csv",
+        headers={"Content-disposition": "attachment"})
 
 @app.route("/logs")
 @login_required
