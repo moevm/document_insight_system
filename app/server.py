7ca565528835d7fc9bfdd1943d057f85ff08fb51
Sasha Kogan <postmoderndeath@gmail.com>
2021-11-30 16:07:08 +0300

    fx

--- app/server.py
+++ app/server.py
@@ -3,7 +3,6 @@ import logging
 from sys import argv
 import pandas as pd
 import json
-import csv
 import bson
 import pymongo
 from bson import ObjectId
@@ -17,6 +16,7 @@ from app.servants import pre_luncher
 from app.servants.user import update_criteria
 from app.root_logger import get_logging_stdout_handler, get_root_logger
 from app.utils.decorators import decorator_assertion
+from app.utils.checklist_filter import checklist_filter
 from app.lti_session_passback.lti.check_request import check_request
 from lti_session_passback.lti import utils
 
@@ -195,78 +195,7 @@ def check_list():
 @app.route("/check_list/data")
 @login_required
 def check_list_data():
-    # transform json filter into dict
-    filters = request.args.get("filter", "{}")
-    try:
-        filters = json.loads(filters)
-        filters = filters if filters else {}
-    except Exception as e:
-        logger.warning("Can't parse filters")
-        logger.warning(repr(e))
-        filters = {}
-
-    # request filter to mongo query filter conversion
-    filter_query = {}
-    if f_filename := filters.get("filename", None):
-        filter_query["filename"] = { "$regex": f_filename }
-
-    if f_user := filters.get("user", None):
-        filter_query["user"] = { "$regex": f_user }
-
-    f_upload_date = filters.get("upload-date", "")
-    f_upload_date_list = list(filter(lambda val: val, f_upload_date.split("-")))
-    try:
-        if len(f_upload_date_list) == 1:
-            date = datetime.strptime(f_upload_date_list[0], "%d.%m.%Y")
-            filter_query["_id"] = {
-                "$gte": ObjectId.from_datetime(date),
-                "$lte": ObjectId.from_datetime(date + timedelta(hours=23, minutes=59, seconds=59))
-            }
-        elif len(f_upload_date_list) > 1:
-            filter_query["_id"] = {
-                "$gte": ObjectId.from_datetime(datetime.strptime(f_upload_date_list[0], "%d.%m.%Y")),
-                "$lte": ObjectId.from_datetime(datetime.strptime(f_upload_date_list[1], "%d.%m.%Y"))
-            }
-    except Exception as e:
-        logger.warning("Can't apply upload-date filter")
-        logger.warning(repr(e))
-
-    f_moodle_date = filters.get("moodle-date", "")
-    f_moodle_date_list = list(filter(lambda val: val, f_moodle_date.split("-")))
-    try:
-        if len(f_moodle_date_list) == 1:
-            date = datetime.strptime(f_moodle_date_list[0], "%d.%m.%Y")
-            filter_query['lms_passback_time'] = {
-                "$gte": date,
-                "$lte": date + timedelta(hours=23, minutes=59, seconds=59)
-            }
-        elif len(f_moodle_date_list) > 1:
-            filter_query['lms_passback_time'] = {
-                "$gte": datetime.strptime(f_moodle_date_list[0], "%d.%m.%Y"),
-                "$lte": datetime.strptime(f_moodle_date_list[1], "%d.%m.%Y")
-            }
-    except Exception as e:
-        logger.warning("Can't apply moodle-date filter")
-        logger.warning(repr(e))
-
-    f_score = filters.get("score", "")
-    f_score_list = list(filter(lambda val: val, f_score.split("-")))
-    try:
-        if len(f_score_list) == 1:
-            filter_query["score"] = float(f_score_list[0])
-        elif len(f_score_list) > 1:
-            filter_query["score"] = {
-                "$gte": float(f_score_list[0]),
-                "$lte": float(f_score_list[1])
-            }
-    except Exception as e:
-        logger.warning("Can't apply score filter")
-        logger.warning(repr(e))
-
-    # set user filter for current non-admin user
-    if not current_user.is_admin:
-        filter_query["user"] = current_user.username
-
+    filter_query = checklist_filter(request)
     # parse and validate rest query
     limit = request.args.get("limit", "")
     limit = int(limit) if limit.isnumeric() else 10
@@ -304,74 +233,7 @@ def check_list_data():
 @app.route("/get_csv")
 @login_required
 def get_csv():
-    filters = request.args.get("filter", "{}")
-    try:
-        filters = json.loads(filters)
-        filters = filters if filters else {}
-    except Exception as e:
-        logger.warning("Can't parse filters")
-        logger.warning(repr(e))
-        filters = {}
-
-    filter_query = {}
-    if f_filename := filters.get("filename", None):
-        filter_query["filename"] = { "$regex": f_filename }
-
-    if f_user := filters.get("user", None):
-        filter_query["user"] = { "$regex": f_user }
-
-    f_upload_date = filters.get("upload-date", "")
-    f_upload_date_list = list(filter(lambda val: val, f_upload_date.split("-")))
-    try:
-        if len(f_upload_date_list) == 1:
-            date = datetime.strptime(f_upload_date_list[0], "%d.%m.%Y")
-            filter_query["_id"] = {
-                "$gte": ObjectId.from_datetime(date),
-                "$lte": ObjectId.from_datetime(date + timedelta(hours=23, minutes=59, seconds=59))
-            }
-        elif len(f_upload_date_list) > 1:
-            filter_query["_id"] = {
-                "$gte": ObjectId.from_datetime(datetime.strptime(f_upload_date_list[0], "%d.%m.%Y")),
-                "$lte": ObjectId.from_datetime(datetime.strptime(f_upload_date_list[1], "%d.%m.%Y"))
-            }
-    except Exception as e:
-        logger.warning("Can't apply upload-date filter")
-        logger.warning(repr(e))
-
-    f_moodle_date = filters.get("moodle-date", "")
-    f_moodle_date_list = list(filter(lambda val: val, f_moodle_date.split("-")))
-    try:
-        if len(f_moodle_date_list) == 1:
-            date = datetime.strptime(f_moodle_date_list[0], "%d.%m.%Y")
-            filter_query['lms_passback_time'] = {
-                "$gte": date,
-                "$lte": date + timedelta(hours=23, minutes=59, seconds=59)
-            }
-        elif len(f_moodle_date_list) > 1:
-            filter_query['lms_passback_time'] = {
-                "$gte": datetime.strptime(f_moodle_date_list[0], "%d.%m.%Y"),
-                "$lte": datetime.strptime(f_moodle_date_list[1], "%d.%m.%Y")
-            }
-    except Exception as e:
-        logger.warning("Can't apply moodle-date filter")
-        logger.warning(repr(e))
-
-    f_score = filters.get("score", "")
-    f_score_list = list(filter(lambda val: val, f_score.split("-")))
-    try:
-        if len(f_score_list) == 1:
-            filter_query["score"] = float(f_score_list[0])
-        elif len(f_score_list) > 1:
-            filter_query["score"] = {
-                "$gte": float(f_score_list[0]),
-                "$lte": float(f_score_list[1])
-            }
-    except Exception as e:
-        logger.warning("Can't apply score filter")
-        logger.warning(repr(e))
-
-    if not current_user.is_admin:
-        filter_query["user"] = current_user.username
+    filter_query = checklist_filter(request)
 
     limit = False
     offset = False
