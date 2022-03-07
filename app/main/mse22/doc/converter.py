4267c42749d4860403b3d8e663d77a2b57ea40f2
ghettodimasta <54914863+ghettodimasta@users.noreply.github.com>
2022-03-07 12:29:30 +0300

    Feature/213 add doc -> docx converter

---
+++ app/main/mse22/doc/converter.py
@@ -0,0 +1,12 @@
+import subprocess
+
+
+class Converter:
+    def __init__(self, path_to_file, out_dir='app/main/mse22/converted_files/'):
+        self._file = path_to_file
+        self._out_dir = out_dir
+        self._filename = path_to_file.split('/')[-1].split('.')[0]
+
+    def convert(self):
+        subprocess.call(['soffice', '--headless', '--convert-to', 'docx', '--outdir', self._out_dir, self._file])
+        return self._out_dir + self._filename + '.docx'
---
+++ app/main/mse22/doc/doc_parser.py
@@ -0,0 +1,24 @@
+from converter import *
+from app.main.mse22.docx.docx_uploader import DocxUploader
+
+
+class DocParser(DocxUploader):
+
+    def __init__(self):
+        super().__init__()
+
+    def parse(self, path_to_doc):
+        conv = Converter(path_to_doc)
+        path_to_docx = conv.convert()
+        print(path_to_docx)
+        return self._upload(path_to_docx)
+        # return path_to_docx
+
+
+def main(args):
+    print(args.filename)
+    parser = DocParser()
+    parser.parse(args.filename)
+    parser.parcing()
+    parser.print_info()
+
