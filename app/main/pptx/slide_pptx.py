2250bea978dab7219a2d2aec6fcd921866881c1e
Dmitry Ivanov <darcenrall@gmail.com>
2022-04-13 00:26:01 +0300

    fix slides enum check
    #227
    
    *for pptx
    odp - troubles

--- app/main/pptx/slide_pptx.py
+++ app/main/pptx/slide_pptx.py
@@ -3,15 +3,18 @@ from pptx.enum.shapes import PP_PLACEHOLDER
 
 
 class SlidePPTX(SlideBasic):
-    def __init__(self, container, w, h):
+    def __init__(self, container, w, h, index=-1):
         SlideBasic.__init__(self, container)
         self.dimensions = [w, h]
         for p in container.placeholders:
             if p.is_placeholder and p.placeholder_format.type == PP_PLACEHOLDER.SLIDE_NUMBER:
-                try:
-                    self.page_number = [int(p.text), int(p.left), int(p.top)]
-                except ValueError:
-                    self.page_number = [-1, -1, -1]
+                if p.text == '‹#›':
+                    self.page_number = [index, -1, -1]
+                else:
+                    try:
+                        self.page_number = [int(p.text), int(p.left), int(p.top)]
+                    except ValueError:
+                        self.page_number = [-1, -1, -1]
         for shape in container.shapes:
             if container.shapes.title:
                 self.title = container.shapes.title.text
