cd7a7c137e58aaa6ef2695f34f78bf4a73630405
jendos2001 <jendos2001@yandex.ru>
2022-04-20 14:05:36 +0300

    added fwq chapters and fixed pageCreator

--- app/main/mse22/document/__main__.py
+++ app/main/mse22/document/__main__.py
@@ -6,6 +6,7 @@ from app.main.mse22.document.document import main as document_main
 def parse_args():
     parser = argparse.ArgumentParser(description="File name")
     parser.add_argument("--filename", type=str, required=True, help="path to .docx file")
+    parser.add_argument("--type", type=str, required=True, help="LR or FWQ")
     parser.set_defaults(func=document_main)
     return parser.parse_args()
 
--- app/main/mse22/document/document.py
+++ app/main/mse22/document/document.py
@@ -5,13 +5,20 @@ from app.main.mse22.document.page_creator import PageCreator
 
 
 class Document:
-    def __init__(self, docx_document, filename):
+    def __init__(self, docx_document, filename, file_type):
         core_props = docx_document.core_properties
         self.info = DocumentInfo(core_props.author, core_props.created, core_props.modified)
-        self.pages = PageCreator().createPageObjects(filename)
+        self.type = file_type
+        self.pages, self.errors = PageCreator().createPageObjects(filename, file_type)
 
     def __str__(self):
-        return "document.Document object:\nPages:\n{0}\nInfo: {1}".format(self.pages, self.info)
+        if not self.errors:
+            s = "The document contains all the basic sections."
+        else:
+            s = "There are no sections in the document: "
+            for i in self.errors:
+                s += i + ", "
+        return s + "\ndocument.Document object:\nPages:\n{0}\nInfo: {1}".format(self.pages, self.info)
 
 
 class DocumentInfo:
@@ -26,15 +33,15 @@ class DocumentInfo:
 
 
 def main(args):
-    docx_document = docx.Document(args.filename)
-    parsed_document = Document(docx_document, args.filename)
-    print(parsed_document)
-    print()
-    if len(parsed_document.pages) == 1:
-        print(*parsed_document.pages[0])
-    else:
+    if args.type == 'LR' or args.type == 'FWQ':
+        docx_document = docx.Document(args.filename)
+        parsed_document = Document(docx_document, args.filename, args.type)
+        print(parsed_document)
+        print()
         for page in parsed_document.pages:
             print(page.header)
             for object in page.pageObjects:
                 if object.type != 'table':
                     print(object.data.text, StyleInfo(object.data.style), '', sep='\n')
+    else:
+        print('Wrong file type!')
