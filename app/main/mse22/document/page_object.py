0bd4870de9888fd52982fee35084b74e8696b4c6
dmitrikuz <54910797+dmitrikuz@users.noreply.github.com>
2022-04-23 06:59:55 +0300

    hierarchy

--- app/main/mse22/document/page_object.py
+++ app/main/mse22/document/page_object.py
@@ -1,24 +1,35 @@
-class PageObjectHeader:
-    pass
+from .style_info import StyleInfo
 
-
-class PageObjectImage:
-    pass
-
-
-class PageObjectTable:
-    pass
-
-
-class PageObjectData:
-    pass
-
-
-class PageObjectTypes:
-    pass
-
-
-class PageObject(PageObjectHeader, PageObjectImage, PageObjectTable, PageObjectData):
-    def __init__(self, type, data):
-        self.type = type
+class PageObject:
+    def __init__(self, object_type, data):
+        self.type = object_type
         self.data = data
+        
+        self.style_info = StyleInfo(data.style)
+        self.image = None
+        self.table = None
+        
+  
+class PageObjectHeader(PageObject):
+    def __init__(self, data):
+        super().__init__(data)
+        self.text = data.text
+
+
+class PageObjectImage(PageObject):
+    def __init__(self, data, image):
+        super().__init__(data)
+        self.image = image
+
+
+class PageObjectTable(PageObject):
+    def __init__(self, data, table):
+        super().__init__(data)
+        self.data_matrix = [[c.text for c in row.cells] for row in table.rows]
+
+
+class PageObjectList(PageObject):
+    def __init__(self, data, list_of_paragraphs):
+        super().__init__(data)
+        self.data_list = list_of_paragraphs
+   
