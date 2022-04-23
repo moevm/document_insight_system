44b849aaaa627a2e207df7d6b60167985abb6892
dmitrikuz <54910797+dmitrikuz@users.noreply.github.com>
2022-04-23 07:16:13 +0300

    args

--- app/main/mse22/document/page_object.py
+++ app/main/mse22/document/page_object.py
@@ -11,25 +11,25 @@ class PageObject:
         
   
 class PageObjectHeader(PageObject):
-    def __init__(self, data):
-        super().__init__(data)
+    def __init__(self, object_type, data):
+        super().__init__(object_type, data)
         self.text = data.text
 
 
 class PageObjectImage(PageObject):
-    def __init__(self, data, image):
-        super().__init__(data)
+    def __init__(self, object_type, data, image=None):
+        super().__init__(object_type, data)
         self.image = image
 
 
 class PageObjectTable(PageObject):
-    def __init__(self, data, table):
-        super().__init__(data)
+    def __init__(self, object_type, table):
+        super().__init__(object_type, table)
         self.data_matrix = [[c.text for c in row.cells] for row in table.rows]
 
 
 class PageObjectList(PageObject):
-    def __init__(self, data, list_of_paragraphs):
-        super().__init__(data)
+    def __init__(self, object_type, data, list_of_paragraphs=None):
+        super().__init__(object_type, data)
         self.data_list = list_of_paragraphs
    
