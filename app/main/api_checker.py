2c016437978bf8cd0878396501c49be4df24206f
Dmitry Ivanov <darcenrall@gmail.com>
2022-04-24 21:46:50 +0300

    upd api_checker

--- app/main/api_checker.py
+++ app/main/api_checker.py
@@ -1,6 +1,7 @@
 from argparse import Namespace
-from app.main.checks import SldNumCheck, SearchKeyWord, FindTasks, FindDefSld, \
-                            SldEnumCheck, SldSimilarity, TitleFormatCheck, FurtherDev
+from app.main.checks import (FindDefSld, FindTasks, FurtherDev, SearchKeyWord,
+                             SldEnumCheck, SldNumCheck, SldSimilarity,
+                             TemplateNameCheck, TitleFormatCheck)
 
 key_slides = {
               'goals_and_tasks': 'Цель и задачи',
@@ -13,17 +14,19 @@ key_slide = Namespace(**key_slides)
 def check(presentation, checks, presentation_name):
     check_names = checks.enabled_checks.keys()
     set_enabled = dict.fromkeys(check_names, False)
-    check_classes = [SldNumCheck(presentation, checks.enabled_checks['slides_number']),
-                     SldEnumCheck(presentation, checks.conv_pdf_fs_id),
-                     TitleFormatCheck(presentation, checks.conv_pdf_fs_id),
-                     FindDefSld(presentation, key_slide.goals_and_tasks, checks.conv_pdf_fs_id),
-                     FindDefSld(presentation, key_slide.approbation, checks.conv_pdf_fs_id),
-                     SearchKeyWord(presentation, key_slide.relevance, checks.conv_pdf_fs_id),
-                     FindDefSld(presentation, key_slide.conclusion, checks.conv_pdf_fs_id),
-                     FindTasks(presentation, key_slide.goals_and_tasks, checks.enabled_checks['slide_every_task']),
-                     SldSimilarity(presentation, key_slide.goals_and_tasks, key_slide.conclusion, checks.enabled_checks['conclusion_actual']),
-                     FurtherDev(presentation, key_slide.goals_and_tasks, key_slide.conclusion, checks.conv_pdf_fs_id)]
-    set_checks = dict(zip(check_names, check_classes))
+    set_checks = {
+        'template_name': TemplateNameCheck(presentation, presentation_name),
+        'slides_number': SldNumCheck(presentation, checks.enabled_checks['slides_number']),
+        'slides_enum': SldEnumCheck(presentation, checks.conv_pdf_fs_id),
+        'slides_headers': TitleFormatCheck(presentation, checks.conv_pdf_fs_id),
+        'goals_slide': FindDefSld(presentation, key_slide.goals_and_tasks, checks.conv_pdf_fs_id),
+        'probe_slide': FindDefSld(presentation, key_slide.approbation, checks.conv_pdf_fs_id),
+        'actual_slide': SearchKeyWord(presentation, key_slide.relevance, checks.conv_pdf_fs_id),
+        'conclusion_slide': FindDefSld(presentation, key_slide.conclusion, checks.conv_pdf_fs_id),
+        'slide_every_task': FindTasks(presentation, key_slide.goals_and_tasks, checks.enabled_checks['slide_every_task']),
+        'conclusion_actual': SldSimilarity(presentation, key_slide.goals_and_tasks, key_slide.conclusion, checks.enabled_checks['conclusion_actual']),
+        'conclusion_along': FurtherDev(presentation, key_slide.goals_and_tasks, key_slide.conclusion, checks.conv_pdf_fs_id)
+    }
     enabled_checks = dict((key, value) for key, value in checks.enabled_checks.items() if value)
 
     for k, v in enabled_checks.items():
