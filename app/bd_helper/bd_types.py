b7391d46023be6c1cbd3ff22fc3742d3636d6cfc
Dmitry Ivanov <darcenrall@gmail.com>
2022-04-24 20:55:01 +0300

    add DEFAULT_CRITERIA

--- app/bd_helper/bd_types.py
+++ app/bd_helper/bd_types.py
@@ -1,4 +1,3 @@
-from app.lti_session_passback.lti.utils import DEFAULT_CRITERIA
 from flask_login import UserMixin
 from app.main.checks_config.parser import sld_num
 
@@ -11,6 +10,11 @@ class Packable:
 
     def __str__(self) -> str:
         return f"{self.__class__.__name__}: {', '.join([f'{key}: {value}' for key, value in vars(self).items()])}"
+
+DEFAULT_CRITERIA = {'template_name': True, 'slides_number': {'sld_num': sld_num['bsc'], 'detect_additional': True},
+                    'slides_enum': True, 'slides_headers': True, 'goals_slide': True, 'probe_slide': True,
+                    'actual_slide': True, 'conclusion_slide': True, 'slide_every_task': 50,
+                    'conclusion_actual': 50, 'conclusion_along': True}
 
 
 # You shouldn't create this or change username and presentations explicitly
