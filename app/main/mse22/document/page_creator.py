ebb7e9450355c2d9beb09efb101ea93984be3efa
Evgeny Kurshev <jendos2001@yandex.ru>
2022-04-29 18:03:17 +0300

    added division by content

--- app/main/mse22/document/page_creator.py
+++ app/main/mse22/document/page_creator.py
@@ -2,7 +2,7 @@ from docx2python import docx2python
 import docx
 import re
 
-from app.main.mse22.document.page_object import PageObjectImage, PageObjectTable, PageObjectList, PageObjectHeader
+from app.main.mse22.document.page_object import PageObjectImage, PageObjectTable, PageObjectHeader
 from app.main.mse22.document.page import Page
 
 
@@ -15,6 +15,36 @@ class PageCreator:
         else:
             return PageObjectHeader('paragraph', docx_docx.paragraphs[paragraph_index])
 
+    @staticmethod
+    def make_content(doc, body_index, paragraph_index):
+        new_chapters = []
+        index = 0
+        if len(doc.body[body_index][0][0]) == paragraph_index + 1:
+            body_index += 1
+            paragraph_index = 0
+        for elem in doc.body[body_index]:
+            if elem[0][0] == 'ЗАКЛЮЧЕНИЕ':
+                break
+            elif elem[0][0] == 'ВВЕДЕНИЕ' or elem[0][0] == 'ОПРЕДЕЛЕНИЯ, ОБОЗНАЧЕНИЯ И СОКРАЩЕНИЯ' or elem[0][0] == '':
+                continue
+            else:
+                new_chapters.append(elem[0][0])
+        else:
+            paragraph_index += 1
+            if 'ЗАКЛЮЧЕНИЕ' not in doc.body[body_index][0][0]:
+                return False, None, None
+            for i in range(paragraph_index, len(doc.body[body_index][0][0])):
+                if doc.body[body_index][0][0][i] == 'ВВЕДЕНИЕ' \
+                        or doc.body[body_index][0][0][i] == 'ОПРЕДЕЛЕНИЯ, ОБОЗНАЧЕНИЯ И СОКРАЩЕНИЯ' \
+                        or doc.body[body_index][0][0][i] == '':
+                    continue
+                elif doc.body[body_index][0][0][i] == 'ЗАКЛЮЧЕНИЕ':
+                    index = i + 1
+                    break
+                else:
+                    new_chapters.append(doc.body[body_index][0][0][i])
+        return True, new_chapters, index
+
     def createPageObjects(self, path, file_type):
         docx_docx2python = docx2python(path)
         indices, errors = self.makeIndices(docx_docx2python, file_type)
@@ -24,11 +54,10 @@ class PageCreator:
         paragraph_index = 0
         page_objects = []
         current_page_object = []
-        chapter = None
 
         for i in range(0, len(indices)):
+            chapter = docx_docx.paragraphs[paragraph_index].text
             for j in range(indices[i][0][0], indices[i][1][0] + 1):
-                chapter = docx_docx.paragraphs[paragraph_index].text
                 if i == 0:
                     chapter = 'Титульный лист'
 
@@ -66,23 +95,22 @@ class PageCreator:
 
         return page_objects, errors
 
-    @staticmethod
-    def makeIndices(doc_result, file_type):
+    def makeIndices(self, doc_result, file_type):
         if file_type == 'LR':
             chapters = ['Цель работы', 'Основные теоретические положения',
                         'Выполнение работы', 'Тестирование', 'Выводы']
         else:
             chapters = ['ЗАДАНИЕ НА ВЫПУСКНУЮ КВАЛИФИКАЦИОННУЮ РАБОТУ',
                         'КАЛЕНДАРНЫЙ ПЛАН ВЫПОЛНЕНИЯ ВЫПУСКНОЙ КВАЛИФИКАЦИОННОЙ РАБОТЫ', 'РЕФЕРАТ', 'ABSTRACT',
-                        'СОДЕРЖАНИЕ', 'ОПРЕДЕЛЕНИЯ, ОБОЗНАЧЕНИЯ И СОКРАЩЕНИЯ', 'ВВЕДЕНИЕ', 'ОСНОВНАЯ ЧАСТЬ',
-                        'ЗАКЛЮЧЕНИЕ', 'СПИСОК ИСПОЛЬЗОВАННЫХ ИСТОЧНИКОВ']
+                        'СОДЕРЖАНИЕ', 'ОПРЕДЕЛЕНИЯ, ОБОЗНАЧЕНИЯ И СОКРАЩЕНИЯ', 'ВВЕДЕНИЕ', 'ЗАКЛЮЧЕНИЕ',
+                        'СПИСОК ИСПОЛЬЗОВАННЫХ ИСТОЧНИКОВ']
         i_start = [0, 0]
         docx_chapters = []
         errors = []
         cur_index = 0
         cur_chapter = 0
         application_pattern = r'ПРИЛОЖЕНИЕ [ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ]{1}'
-
+        find_index = 0
         while cur_chapter != len(chapters):
             if cur_index == len(doc_result.body):
                 if chapters[cur_chapter] != 'ОПРЕДЕЛЕНИЯ, ОБОЗНАЧЕНИЯ И СОКРАЩЕНИЯ':
@@ -100,14 +128,35 @@ class PageCreator:
             if len(doc_result.body[cur_index]) > 1:  # table
                 cur_index += 1
 
-            elif chapters[cur_chapter] in doc_result.body[cur_index][0][0]:
+            elif chapters[cur_chapter] in doc_result.body[cur_index][0][0][i_start[1]:]:
+                if chapters[cur_chapter] == 'СОДЕРЖАНИЕ':
+                    is_correct, new_chapters, index = self.make_content(doc_result, cur_index,
+                                                                        doc_result.body[cur_index][0][0]
+                                                                        .index(chapters[cur_chapter]))
+                    if is_correct:
+                        chapters = chapters[:cur_chapter + 3] + new_chapters + chapters[cur_chapter + 3:]
+
+                        if not docx_chapters:
+                            docx_chapters.append([i_start, [cur_index, doc_result.body[cur_index][0][0].
+                                                 index(chapters[cur_chapter]) - 1]])
+                        else:
+                            docx_chapters.append([[docx_chapters[-1][1][0], docx_chapters[-1][1][1] + 1],
+                                                  [cur_index,
+                                                   doc_result.body[cur_index][0][0].index(chapters[cur_chapter]) - 1]])
+
+                        find_index = index
+                        i_start = [docx_chapters[-1][1][0], docx_chapters[-1][1][1] + 1]
+                        cur_chapter += 1
+                        continue
+
                 tmp_i_end = [0, 0]
                 tmp_i_start = i_start.copy()
                 tmp_i_end[0] = cur_index
-                tmp_i_end[1] = doc_result.body[cur_index][0][0].index(chapters[cur_chapter]) - 1
+                tmp_i_end[1] = doc_result.body[cur_index][0][0].index(chapters[cur_chapter], find_index) - 1
                 docx_chapters.append([tmp_i_start, tmp_i_end])
                 i_start[0], i_start[1] = tmp_i_end[0], tmp_i_end[1] + 1
                 cur_chapter += 1
+
             else:
                 cur_index += 1
         if not docx_chapters:
