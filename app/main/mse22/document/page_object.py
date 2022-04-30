8feba69dbd19184c03d9c26e3c88cdbf9610a789
dmitrikuz <54910797+dmitrikuz@users.noreply.github.com>
2022-04-30 10:15:11 +0300

    update page objects

--- app/main/mse22/document/page_object.py
+++ app/main/mse22/document/page_object.py
@@ -6,7 +6,6 @@ class PageObject:
         self.data = data
         
         self.style_info = StyleInfo(data.style)
-        self.image = None
         self.table = None
         
   
@@ -17,15 +16,14 @@ class PageObjectHeader(PageObject):
 
 
 class PageObjectImage(PageObject):
-    def __init__(self, object_type, data, image=None):
+    def __init__(self, object_type, data):
         super().__init__(object_type, data)
-        self.image = image
 
 
 class PageObjectTable(PageObject):
     def __init__(self, object_type, table):
         super().__init__(object_type, table)
-        self.data_matrix = [[c.text for c in row.cells] for row in table.rows]
+        self.data_matrix = [[c.paragraphs for c in row.cells] for row in table.rows]
 
 
 class PageObjectList(PageObject):
