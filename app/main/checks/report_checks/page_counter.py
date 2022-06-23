ec529bc817dfd80d7ff85d6d4fd5f0d28d90c140
Maria <sorochina_m@mail.ru>
2022-06-23 17:03:19 +0300

    delete prints, change answer, fix flags

--- app/main/checks/report_checks/page_counter.py
+++ app/main/checks/report_checks/page_counter.py
@@ -9,16 +9,17 @@ class ReportNumberOfPages(BaseCheck):
         self.maxNumber = maxNumber
 
     def check(self):
-        print(type(self.file.paragraphs[0]))
         count = 0
         for k, v in self.file.pdf_file.text_on_page.items():
-            print(f"Страница №{k}", end='\n\n')
             if re.search('приложение [а-я][\n .]', v.lower()):
                 break
             count += 1
 
         if count >= self.minNumber and (self.maxNumber is None or count <= self.maxNumber):
             return answer(True, f"Пройдена! {count} страниц")
-        return answer(False, f'Неверное количество страниц в файле. ({count} страниц)')
+        if self.maxNumber:
+            return answer(False, f'Неверное количество страниц в файле: должно быть [{self.minNumber}, {self.maxNumber}] страниц, в отчете {count} страниц')
+        else:
+            return answer(False, f'Неверное количество страниц в файле: должно быть не менее {self.minNumber} страниц, в отчете {count} страниц')
 
 
