d6d64b3cb2b818157cbad103ec9341fcc855aa16
Maria <sorochina_m@mail.ru>
2022-07-09 01:44:18 +0300

    fix regexp & literature counter

--- app/main/checks/report_checks/literature_references.py
+++ app/main/checks/report_checks/literature_references.py
@@ -10,9 +10,9 @@ class ReferencesToLiteratureCheck(BaseReportCriterion):
         super().__init__(file_info)
 
     def check(self):
-        start_literature_par, end_literature_par, empty_strings = self.find_start_and_end_paragraphs()
+        start_literature_par= self.find_start_paragraph()
         if start_literature_par:
-            number_of_sources = end_literature_par - start_literature_par - empty_strings - 1
+            number_of_sources = self.count_sources()
             references = self.search_references(start_literature_par)
             if len(references) == number_of_sources:
                 return answer(True, f"Пройдена!")
@@ -41,23 +41,51 @@ class ReferencesToLiteratureCheck(BaseReportCriterion):
                             array_of_references.add(int(one_part))
         return array_of_references
 
-    def find_start_and_end_paragraphs(self):
+    def find_start_paragraph(self):
         start_index = 0
-        end_index = len(self.file.paragraphs)
-        empty_strings = 0
         for i in range(len(self.file.paragraphs)):
             text_string = self.file.paragraphs[i].to_string().lower().split('\n')[1]
-            if re.fullmatch(r'text\s+список использованных источников[\s.]+', text_string) or \
-                    re.fullmatch(r'text\s+список использованной литературы[\s.]+', text_string) or \
-                    re.fullmatch(r'text\s+список литературы[\s.]+', text_string):
+            if re.fullmatch(r'text\s+список[ \t]*(использованных|использованной|)[ \t]*(источников|литературы).?\s*',
+                            text_string):
                 start_index = i
                 break
-        if start_index:
-            for i in range(start_index, len(self.file.paragraphs)):
-                text_string = self.file.paragraphs[i].to_string().lower().split('\n')[1]
-                if re.fullmatch(r'text[\s\\t]+', text_string):
-                    empty_strings += 1
-                if re.fullmatch(r'text\s+приложение а.?\s*', text_string):
-                    end_index = i
+        return start_index
+
+    def count_sources(self):
+        literature_counter = 0
+        start_page, end_page = self.search_literature_start_pdf()
+        print(f"start page = {start_page}, end page = {end_page}")
+        for i in range(start_page, end_page + 1):
+            print(f"i = {i}")
+            one_page = self.file.pdf_file.text_on_page[i].split('\n')
+            first_string = -1
+            last_string = len(one_page)
+
+            for j in range(len(one_page)):
+                one_str_lowercase = one_page[j].lower()
+                if re.search(r'\s+список[ \t]*(использованных|использованной|)[ \t]*(источников|литературы).?\s*\n',
+                             one_str_lowercase):
+                    first_string = j
                     break
-        return start_index, end_index, empty_strings
+            for j in range(first_string, len(one_page)):
+                if re.search('приложение а[\n .]', one_page[j].lower()):
+                    last_string = j
+                    break
+
+            for ind in range(first_string + 1, last_string):
+                if re.match(f"{literature_counter + 1}.", one_page[ind]):
+                    literature_counter += 1
+        return literature_counter
+
+    def search_literature_start_pdf(self):
+        start_page = 0
+        end_page = self.file.pdf_file.page_count
+        for i in self.file.pdf_file.text_on_page.keys():
+            lowercase_str = self.file.pdf_file.text_on_page[i].lower()
+            if re.search(r'\s*список[ \t]*(использованных|использованной|)[ \t]*(источников|литературы).?\s*',
+                         lowercase_str):
+                start_page = i
+            if re.search('приложение а[\n .]', lowercase_str):
+                end_page = i
+                break
+        return start_page, end_page
