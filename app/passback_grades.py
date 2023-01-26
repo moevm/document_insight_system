509f07e84e2a5a1ec678f399debf3da7f1745616
Dmitry Ivanov <darcenrall@gmail.com>
2023-01-26 18:04:18 +0300

    set passbacked flag for users without passback_params

--- app/passback_grades.py
+++ app/passback_grades.py
@@ -19,7 +19,8 @@ class ChecksPassBack:
     def grade_passback(self, check):
         user = check.get('user')
         passback_params = get_user(user).params_for_passback
-        if not passback_params:
+        if not passback_params or passback_params["lis_outcome_service_url"] == "lis_outcome_service_url":
+            set_passbacked_flag(check.get('_id'), None)
             return
 
         consumer_secret = ConsumersDBManager.get_secret(passback_params['oauth_consumer_key'])
@@ -31,9 +32,10 @@ class ChecksPassBack:
                                                                                                check.get('_id')))
             set_passbacked_flag(check.get('_id'), True)
         else:
-            logger.warning('Passback failed for check_id = {}'.format(check.get('_id')))
+            logger.error('Passback failed for check_id = {}'.format(check.get('_id')))
 
     def _run(self):
+        logger.info('Start passback')
         for check in get_unpassed_checks():
             self.grade_passback(check)
 
