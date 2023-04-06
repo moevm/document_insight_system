616d2745e1968d5aca7a96b9ac6e956eb36cc938
dvivanov <dvivanov@etu.ru>
2023-04-06 15:56:39 +0300

    update lms_passback_time logic

--- app/db/db_methods.py
+++ app/db/db_methods.py
@@ -214,9 +214,13 @@ def get_unpassed_checks():
 def set_passbacked_flag(checks_id, flag):
     upd_check = {"$set": {}}
     upd_check['$set']['is_passbacked'] = flag
-    if not (flag is None):
-        # None - if user without passback
+
+    if flag is None:
+        # flag = None - if user without passback
         upd_check['$set']['lms_passback_time'] = None
+    elif flag:
+        upd_check['$set']['lms_passback_time'] = datetime.now()
+
     check = checks_collection.update_one({'_id': checks_id}, upd_check)
     return check if check else None
 
