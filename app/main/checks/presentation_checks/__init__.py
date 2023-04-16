460676c2b053758b84f9e9d81ca0aaef3b7938cb
Давыдов Михаил <71800457+dart-mih@users.noreply.github.com>
2023-04-16 22:23:35 +0300

    feat: init pres_right_words check

--- app/main/checks/presentation_checks/__init__.py
+++ app/main/checks/presentation_checks/__init__.py
@@ -7,3 +7,4 @@ from .sld_num import SldNumCheck
 from .sld_similarity import SldSimilarity
 from .template_name import TemplateNameCheck
 from .title_format import TitleFormatCheck
+from .pres_right_words import PresRightWordsCheck
