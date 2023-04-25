b9fa58938f624a654e478953ea5d861a89f12d84
Saprigenie <Slava.paramonov.2002@yandex.ru>
2023-04-26 00:35:06 +0300

    fix: added removal of generated temporary converted files

--- app/tasks.py
+++ app/tasks.py
@@ -41,7 +41,11 @@ def create_task(self, check_info):
     original_filepath = join(FILES_FOLDER, f"{check_id}.{check_obj.filename.rsplit('.', 1)[-1]}")
     pdf_filepath = join(FILES_FOLDER, f"{check_id}.pdf")
     try:
-        updated_check = check(parse(original_filepath), check_obj)
+        parsed_files, tmp_file_path = parse(original_filepath)
+        # remove temporary converted to another format file.
+        if tmp_file_path:
+            remove_files([tmp_file_path])
+        updated_check = check(parsed_files, check_obj)
         updated_check.is_ended = True
         updated_check.is_failed = False
         db_methods.update_check(updated_check)  # save to db
