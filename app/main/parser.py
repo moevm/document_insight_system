15c583eb50d77979ef0a0a08542d04d69884f1d8
Dmitry Ivanov <darcenrall@gmail.com>
2023-05-19 13:09:12 +0300

    update filename analyze in document parser

--- app/main/parser.py
+++ app/main/parser.py
@@ -9,19 +9,20 @@ logger = logging.getLogger('root_logger')
 
 
 def parse(filepath, pdf_filepath):
-    if filepath.endswith('.ppt') or filepath.endswith('.pptx'):
+    tmp_filepath = filepath.lower()
+    if tmp_filepath.endswith('.ppt') or tmp_filepath.endswith('.pptx'):
         try:
             return PresentationPPTX(filepath)
         except Exception as err:
             logger.error(err, exc_info=True)
             return None
-    elif filepath.endswith('.odp'):
+    elif tmp_filepath.endswith('.odp'):
         try:
             return PresentationODP(filepath)
         except Exception as err:
             logger.error(err, exc_info=True)
             return None
-    elif filepath.endswith('.doc') or filepath.endswith('.odt'):
+    elif tmp_filepath.endswith('.doc') or tmp_filepath.endswith('.odt'):
         try:
             converted_file_path = convert_to(filepath, target_format='docx')
             docx = DocxUploader()
@@ -32,7 +33,7 @@ def parse(filepath, pdf_filepath):
             logger.error(err, exc_info=True)
             return None
 
-    elif filepath.endswith('.docx'):
+    elif tmp_filepath.endswith('.docx'):
         try:
             docx = DocxUploader()
             docx.upload(filepath, pdf_filepath)
@@ -42,7 +43,7 @@ def parse(filepath, pdf_filepath):
             logger.error(err, exc_info=True)
             return None
     else:
-        raise ValueError("Файл с недопустимым именем или недопустимого формата: " + filename)
+        raise ValueError("Файл с недопустимым именем или недопустимого формата: " + filepath)
 
 def save_to_temp_file(file):
     temp_file = tempfile.NamedTemporaryFile(delete=False)
