c27b2dc5573c6676461153a709575adf27dacb3d
Marina <verika_iz_viro@mail.ru>
2023-06-11 16:49:22 +0300

    try to fix parsing

--- app/main/checks/presentation_checks/if_github.py
+++ app/main/checks/presentation_checks/if_github.py
@@ -1,11 +1,12 @@
 import re
 
-import lxml
-import requests
 from lxml import html
+import requests
+
 
 from ..base_check import BasePresCriterion, answer
-from app.utils.parse_for_html import format_header
+# from app.utils.parse_for_html import format_header
+
 
 class PresIfRefGitHubWork(BasePresCriterion):
     description = "Проверка действительности ссылки на github"
@@ -20,27 +21,33 @@ class PresIfRefGitHubWork(BasePresCriterion):
         for page, slide in enumerate(self.file.get_text_from_slides(), 1):
             text_from_slide.append(slide)
             string_from_text = ''.join(text_from_slide)
-        result = re.findall(r'(((((http(s)?://)?(github|gitlab|bitbucket)+)+(.com|.org)+)+/\w+)+/\w+)+', string_from_text)
+        result = re.findall(
+            r'((((((http(s)?://)?(github|gitlab|bitbucket)+)+(.com|.org)+)+/[a-zA-Z0-9_-]+)+/[a-zA-Z0-9_-]+)+/*)+',
+            string_from_text)
         if not result:
             return answer(True, 'Нечего проверять!')
         else:
             for i in result:
                 try:
-                    page = requests.get(i[0])
-                    if page.status_code != 200:
+                    link = requests.get(i[0])
+                    if link.status_code != 200:
                         raise requests.exceptions.ConnectionError
-                    tree = lxml.html.fromstring(page.content)
-                    file_and_folders = tree.xpath('//*[@class="js-navigation-open"]')
-                    if not file_and_folders:
+                    tree = html.fromstring(link.content)
+                    if not tree.xpath("//a[contains(concat(' ',normalize-space(@class),' '),' tree-item-link ')][contains(@href,'/tree/master/')]/text()"):
+                    # @ class ='js-navigation-open Link--primary' or
                         empty_repo_ref.append(i[0])
                 except (requests.exceptions.SSLError, requests.exceptions.ConnectionError):
                     wrong_repo_ref.append(i[0])
-            if wrong_repo_ref:
-                string_result += f"Найдены некорректные ссылки на репозитории! {wrong_repo_ref}"
-                if empty_repo_ref:
-                    string_result += f", Также Найдены пустые репозитории! {empty_repo_ref}"
-            elif empty_repo_ref:
-                string_result += f"Найдены пустые репозитории! {empty_repo_ref}"
+            if wrong_repo_ref and not empty_repo_ref:
+                string_result += f"Найдены некорректные ссылки на репозитории! {wrong_repo_ref} ВСЕГО {result}"
+                check_result = False
+            elif empty_repo_ref and not wrong_repo_ref:
+                string_result += f"Найдены пустые репозитории! {empty_repo_ref} ВСЕГО {result}"
+                check_result = False
+            elif empty_repo_ref and wrong_repo_ref:
+                string_result += f"Найдены пустые репозитории! {empty_repo_ref} Также Найдены некорректные репозитории! {wrong_repo_ref} ВСЕГО {result}"
+                check_result = False
             else:
                 string_result = 'Пройдена!'
-            return answer(True, string_result)
+                check_result = True
+            return answer(check_result, string_result)
