14bdc860848f0a0c43c688c50434856b59ce3de3
Saprigenie <Slava.paramonov.2002@yandex.ru>
2023-07-03 23:55:59 +0300

    fix: restored conversion functionality after merge

--- app/main/parser.py
+++ app/main/parser.py
@@ -13,20 +13,23 @@ logger = logging.getLogger('root_logger')
 def parse(filepath, pdf_filepath):
     tmp_filepath = filepath.lower()
     try:
-        new_filepath = filepath
-        if tmp_filepath.endswith('.odp'):
-            new_filepath = convert_to(filepath, target_format='pptx')
-        elif tmp_filepath.endswith('.ppt') or tmp_filepath.endswith('.pptx'):
-            file_object = PresentationPPTX(new_filepath)
-        else:
+        if tmp_filepath.endswith(('.odp', '.ppt', '.pptx')):
             new_filepath = filepath
-            if tmp_filepath.endswith('.doc') or tmp_filepath.endswith('.odt'):
+            if tmp_filepath.endswith(('.odp', '.ppt')):
+                logger.info(f"Презентация {filepath} старого формата. Временно преобразована в pptx для обработки.")
+                new_filepath = convert_to(filepath, target_format='pptx')
+            file_object = PresentationPPTX(new_filepath)
+        elif tmp_filepath.endswith(('.doc', '.odt', '.docx')):
+            new_filepath = filepath
+            if tmp_filepath.endswith(('.doc', '.odt')):
+                logger.info(f"Отчёт {filepath} старого формата. Временно преобразован в docx для обработки.")
                 new_filepath = convert_to(filepath, target_format='docx')
-            elif tmp_filepath.endswith('.docx'):
-                docx = DocxUploader()
-                docx.upload(new_filepath, pdf_filepath)
-                docx.parse()
-                file_object = docx
+            docx = DocxUploader()
+            docx.upload(new_filepath, pdf_filepath)
+            docx.parse()
+            file_object = docx
+        else:
+            raise ValueError("Файл с недопустимым именем или недопустимого формата: " + filepath)
         # Если была конвертация, то удаляем временный файл.
         if new_filepath != filepath:
             os.remove(new_filepath)
