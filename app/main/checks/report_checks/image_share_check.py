d9d6e6ae050ab78d3954adfbd782045a453283e5
Marina <verika_iz_viro@mail.ru>
2023-07-04 12:16:14 +0300

    fix image share check

--- app/main/checks/report_checks/image_share_check.py
+++ app/main/checks/report_checks/image_share_check.py
@@ -1,6 +1,5 @@
 from ..base_check import BaseReportCriterion, answer
 
-
 class ReportImageShareCheck(BaseReportCriterion):
     description = "Проверка доли объема отчёта, приходящейся на изображения"
     id = 'image_share_check'
@@ -12,27 +11,25 @@ class ReportImageShareCheck(BaseReportCriterion):
     def check(self):
         if self.file.page_counter() < 4:
             return answer(False, "В отчете недостаточно страниц. Нечего проверять.")
-        images_height = 0
-        for image in self.file.inline_shapes:
-            images_height += image.height.cm
-        if len(self.file.file.sections):
-            available_space = self.file.file.sections[0].page_height.cm - self.file.file.sections[0].bottom_margin.cm - \
-                              self.file.file.sections[0].top_margin.cm
-            images_pages = images_height / available_space
-            share = images_pages / self.file.count
-            if share > self.limit:
-                result_str = f'Проверка не пройдена! Изображения в работе занимают около {round(share, 2)} объема ' \
-                             f'документа без учета приложения, ограничение - {round(self.limit, 2)}'
-                result_str += '''
-                            Если доля отчета, приходящаяся на изображения, больше нормы, попробуйте сделать следующее:
-                            <ul>
-                                <li>Попробуйте перенести малозначимые иллюстрации в Приложение;</li>
-                                <li>Если у вас уже есть раздел Приложение, убедитесь, что количество страниц в отчете посчитано программой без учета приложения;</li>
-                                <li>Если страницы посчитаны программой неверно, убедитесь, что заголовок приложения правильно оформлен;</li>
-                                <li>Убедитесь, что красная строка не сделана с помощью пробелов или табуляции.</li>
-                            </ul>
-                            '''
-                return answer(False, result_str)
-            else:
-                return answer(True, f'Пройдена!')
-        return answer(False, 'Во время обработки произошла критическая ошибка')
+        images_height = self.file.pdf_file.page_images()
+        available_space = self.file.pdf_file.page_height()
+
+        images_value = images_height/available_space
+
+        if images_value > self.limit:
+            result_str = f'Проверка не пройдена! Изображения в работе занимают около {round(images_value, 2)} объема ' \
+                         f'документа без учета приложения, ограничение - {round(self.limit, 2)}'
+            result_str += '''
+                        Если доля отчета, приходящаяся на изображения, больше нормы, попробуйте сделать следующее:
+                        <ul>
+                            <li>Попробуйте перенести малозначимые иллюстрации в Приложение;</li>
+                            <li>Если у вас уже есть раздел Приложение, убедитесь, что количество страниц в отчете посчитано программой без учета приложения;</li>
+                            <li>Если страницы посчитаны программой неверно, убедитесь, что заголовок приложения правильно оформлен;</li>
+                            <li>Убедитесь, что красная строка не сделана с помощью пробелов или табуляции.</li>
+                        </ul>
+                        '''
+            return answer(False, result_str)
+        else:
+            return answer(True, f'Пройдена!')
+
+        # return answer(False, f'Во время обработки произошла критическая ошибка')
--- app/main/reports/pdf_document/pdf_document_manager.py
+++ app/main/reports/pdf_document/pdf_document_manager.py
@@ -1,14 +1,18 @@
+
 import pdfplumber
+import fitz
+
 
 from app.utils import convert_to
-
 
 class PdfDocumentManager:
     def __init__(self, path_to_file, pdf_filepath=''):
         if not pdf_filepath:
             self.pdf_file = pdfplumber.open(convert_to(path_to_file, target_format='pdf'))
+            self.pdf_fitz = fitz.open(convert_to(path_to_file, target_format='pdf'))
         else:
             self.pdf_file = pdfplumber.open(pdf_filepath)
+            self.pdf_fitz = fitz.open(pdf_filepath)
         self.pages = self.pdf_file.pages
         self.page_count = len(self.pages)
         self.text_on_page = self.get_text_on_page()
@@ -17,6 +21,27 @@ class PdfDocumentManager:
 
     def get_text_on_page(self):
         return {page + 1: self.pages[page].extract_text() for page in range(self.page_count)}
+
+    def page_images(self):
+        total_height = 0
+        for page_num in range(self.page_count):
+            page = self.pdf_fitz[page_num]
+            images = self.pdf_fitz.get_page_images(page)
+            for image in images:
+                image_coord = page.get_image_bbox(image[7], transform=0)
+                total_height += (image_coord[3] - image_coord[1])
+
+        return total_height
+
+    def page_height(self):
+        page = self.pdf_fitz[0]   # get first page as a sample
+        page_rect = page.rect
+        height = page_rect.height
+        top_margin = page_rect.y0
+        bottom_margin = height - page_rect.y1
+        available_space = (height - top_margin - bottom_margin)*self.page_count
+
+        return available_space
 
     # def get_only_text_on_page(self):
     #     if not self.only_text_on_page:
