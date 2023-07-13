eb7c976350e3a409d09a48043133e53383c611cc
Marina <verika_iz_viro@mail.ru>
2023-07-13 12:54:53 +0300

    different check for pptx and odp

--- app/main/checks/presentation_checks/empty_slide_check.py
+++ app/main/checks/presentation_checks/empty_slide_check.py
@@ -25,7 +25,7 @@ class PresEmptySlideCheck(BasePresCriterion):
         for page, slide in enumerate(self.file.get_text_from_slides(), 1):
             slide_string = ''.join(slide.replace("\n", " "))
             slide_without_page = re.sub(r'\d+(?=\s*$)', '', slide_string)
-            print(slide_without_page)
+            print(slide_without_page, self.file.get_text_from_slides)
             full_pages[str(page)] = ''.join(char for char in slide_without_page.strip() if char.isprintable())
             if not full_pages[str(page)]:
                 empty_pages.append(page)
@@ -35,21 +35,30 @@ class PresEmptySlideCheck(BasePresCriterion):
             if slide == full_pages[str(page)] and page not in pages_with_images and page not in empty_pages:
                 pages_with_title.append(page)
 
-        if empty_pages and not pages_with_title:
-            result_str += format_header(
-                'Не пройдена! Обнаружены пустые слайды: {}'.format(
-                    ', '.join(self.format_page_link(empty_pages)))
-            )
-        if pages_with_title and not empty_pages:
-            result_str += format_header(
-                'Не пройдена! Обнаружены слайды, в которых присутствует только заголовок: {}'.format(
-                    ', '.join(self.format_page_link(pages_with_title)))
-            )
-        if empty_pages and pages_with_title:
-            result_str += format_header(
-                'Не пройдена! Обнаружены пустые слайды: {}, также обнаружены слайды, в которых присутствует только заголовок: {}'.format(
-                    ', '.join(self.format_page_link(empty_pages)), ', '.join(self.format_page_link(pages_with_title)))
-            )
+        if self.file.presentation_name.endswith('.ppt') or self.file.presentation_name.endswith('.pptx'):
+
+            if empty_pages and not pages_with_title:
+                result_str += format_header(
+                    'Не пройдена! Обнаружены пустые слайды: {}'.format(
+                        ', '.join(self.format_page_link(empty_pages)))
+                )
+            if pages_with_title and not empty_pages:
+                result_str += format_header(
+                    'Не пройдена! Обнаружены слайды, в которых присутствует только заголовок: {}'.format(
+                        ', '.join(self.format_page_link(pages_with_title)))
+                )
+            if empty_pages and pages_with_title:
+                result_str += format_header(
+                    'Не пройдена! Обнаружены пустые слайды: {}, также обнаружены слайды, в которых присутствует только заголовок: {}'.format(
+                        ', '.join(self.format_page_link(empty_pages)), ', '.join(self.format_page_link(pages_with_title)))
+                )
+        elif self.file.presentation_name.lower().endswith('.odp'):
+            if empty_pages:
+                result_str += format_header(
+                    'Не пройдена! Обнаружены пустые слайды или слайды, в которых присутствует только заголовок: {}'.format(
+                        ', '.join(self.format_page_link(empty_pages)))
+                )
+
         if not result_str:
             self.status = True
             result_str = 'Пройдена!'
