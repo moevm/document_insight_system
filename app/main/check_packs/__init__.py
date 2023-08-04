4388efaf0f57798d19c3135fefb55762e38ec3af
Marina <verika_iz_viro@mail.ru>
2023-08-04 19:17:24 +0300

    firt kind of check_info

--- app/main/check_packs/__init__.py
+++ app/main/check_packs/__init__.py
@@ -1,4 +1,4 @@
 from .base_criterion_pack import BaseCriterionPack
 from .pack_config import BASE_PACKS, DEFAULT_TYPE, DEFAULT_TYPE_INFO, DEFAULT_PRES_TYPE_INFO, DEFAULT_REPORT_TYPE_INFO, \
-    REPORT_TYPES
+    REPORT_TYPES, BASE_PRES_CRITERION, BASE_REPORT_CRITERION
 from .utils import init_criterions
