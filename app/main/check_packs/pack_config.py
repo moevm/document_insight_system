1aab2c61dbae9676bd27b80833a8bc74a2a9347d
Marina <verika_iz_viro@mail.ru>
2023-09-19 14:47:20 +0300

    fix button bug

--- app/main/check_packs/pack_config.py
+++ app/main/check_packs/pack_config.py
@@ -45,6 +45,7 @@ BASE_REPORT_CRITERION = [
 DEFAULT_TYPE = 'pres'
 REPORT_TYPES = ('LR', 'VKR')
 DEFAULT_REPORT_TYPE_INFO = {'type': 'report', 'report_type': REPORT_TYPES[1]}
+# DEFAULT_REPORT_TYPE_INFO = {'type': 'report', 'report_type': REPORT_TYPES[0]}
 DEFAULT_PRES_TYPE_INFO = {'type': 'pres'}
 DEFAULT_TYPE_INFO = DEFAULT_PRES_TYPE_INFO
 
@@ -52,5 +53,7 @@ BASE_PACKS = {
     'pres': BaseCriterionPack(BASE_PRES_CRITERION, DEFAULT_PRES_TYPE_INFO, min_score=1.0,
                               name="BasePresentationCriterionPack"),
     'report': BaseCriterionPack(BASE_REPORT_CRITERION, DEFAULT_REPORT_TYPE_INFO, min_score=1.0,
-                                name="BaseReportCriterionPack")
+                                name="BaseReportCriterionPack ")
+    # 'report': BaseCriterionPack(BASE_REPORT_CRITERION, DEFAULT_REPORT_TYPE_INFO, min_score=1.0,
+    #                             name="LabReportPack ")
 }
