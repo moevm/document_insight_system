f80330a989ba61583956a92a1e62fc28fdc4894e
Marina <verika_iz_viro@mail.ru>
2023-09-21 11:10:07 +0300

    change structure

--- app/main/reports/md_parser/md_parser.py
+++ app/main/reports/md_parser/md_parser.py
@@ -2,33 +2,32 @@ import markdown
 import re
 
 class MdParser:
-    def __init__(self):
-        self.path_to_md_file = '' #insert path to file
+    def __init__(self, path_to_md_file):
+        self.path_to_md_file = path_to_md_file
         self.headers = []
         self.chapters = []
         self.paragraphs = []
         self.html_text = ''
         self.tables = []
         self.chapter_with_text = []
-        self.literature_header = []
-        self.headers_page = 0
+        # self.literature_header = ''
 
-    def get_html_from_md(self):
+    def read_md_file(self):
         with open(self.path_to_md_file, "r", encoding="utf-8") as f:
-            text = f.read()
-            self.html_text = markdown.markdown(text)
+            md_text = f.read()
+            return md_text
+
+    def get_html_from_md(self, md_text):
+            self.html_text = markdown.markdown(md_text)
             self.paragraphs = self.html_text.split('\n')
-            return(self.paragraphs, self.html_text)
 
     def get_headers(self):
         header_regex = "<h1>(.*?)<\/h1>"
         self.headers = re.findall(header_regex, self.html_text)
-        return self.headers
 
     def get_chapters(self):
         chapter_regex = "<h2>(.*?)<\/h2>"
         self.chapters = re.findall(chapter_regex, self.html_text)
-        return(self.chapters)
     
     def get_chapter_with_text(self):
         text = self.html_text
@@ -38,9 +37,7 @@ class MdParser:
             self.chapter_with_text.append(chapter_name + self.split_chapter[-2])
             chapter_name = chapter
             text = self.split_chapter[-1]
-            if chapter == self.chapters[len(self.chapters)-1]:
-                self.chapter_with_text.append(chapter_name + text)
-        return self.chapter_with_text
+        self.chapter_with_text.append(chapter_name + text)
     
     def get_tables_size(self):
         count_table_line = 0
@@ -48,13 +45,17 @@ class MdParser:
         for line in self.paragraphs:
             if "|" in line:
                 count_table_line +=1
-        return (count_table_line, count_table_line/count_paragraph)    
+        return count_table_line/count_paragraph
+    
+    def parse_md_file(self):
+        md_text = self.read_md_file()
+        self.get_html_from_md(md_text)
+        self.get_headers()
+        self.get_chapters()
+        self.get_chapter_with_text()
+        self.get_tables_size()
+        return f"{self.headers} \n {self.chapters} \n {self.chapter_with_text} \n {self.get_tables_size()}"
 
-
-            
-file = MdParser()
-file.get_html_from_md()
-# print(file.get_headers())
-# print(file.get_chapters())
-# print(file.get_chapter_with_text())
-print(file.get_tables_size())
+         
+md_file = MdParser('')
+print(md_file.parse_md_file())
