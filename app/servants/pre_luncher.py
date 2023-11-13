a88c5439dfc1cc9ccecd035af01ee78684a9a376
Marina <verika_iz_viro@mail.ru>
2023-11-13 21:48:38 +0300

    fix criteria_info for pres

--- app/servants/pre_luncher.py
+++ app/servants/pre_luncher.py
@@ -35,8 +35,8 @@ def init(app, debug):
         user.is_admin = True
         edit_user(user)
 
-    user.file_type = DEFAULT_PRES_TYPE_INFO
-    file_type = DEFAULT_PRES_TYPE_INFO['type']
+    user.file_type = DEFAULT_REPORT_TYPE_INFO
+    file_type = DEFAULT_REPORT_TYPE_INFO['type']
     user.criteria = BASE_PACKS[file_type].name
     user.formats = list(ALLOWED_EXTENSIONS.get(file_type))
     user.two_files = True
