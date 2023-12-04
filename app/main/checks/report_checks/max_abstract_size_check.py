f9a8dd62dd7ee2625461d8dcd57cfed46ac70c26
Anton Toropygin <antontoropygin4@gmail.com>
2023-12-04 22:35:05 +0300

    add-check-for-both

--- app/main/checks/report_checks/max_abstract_size_check.py
+++ app/main/checks/report_checks/max_abstract_size_check.py
@@ -8,14 +8,13 @@ class ReportMaxSizeOfAbstractCheck(BaseReportCriterion):
     def __init__(self, file_info):
         super().__init__(file_info)
         self.headers = []
+        self.referat_size = 0
+        self.abstract_size = 0
         self.max_size = 0
 
     def late_init(self):
         self.headers = self.file.make_headers(self.file_type['report_type'])
         self.max_size = 1
-
-    def check(self):
-        self.late_init()
         referat_page = 0
         abstract_page = 0
         main_page = 0
@@ -26,13 +25,19 @@ class ReportMaxSizeOfAbstractCheck(BaseReportCriterion):
                 abstract_page = header["page"]
             if header["name"] == "Содержание":
                 main_page = header["page"]
-        referat_size = abstract_page - referat_page
-        abstract_size = main_page - abstract_page
-        if referat_size > self.max_size:
+        self.referat_size = abstract_page - referat_page
+        self.abstract_size = main_page - abstract_page
+
+    def check(self):
+        self.late_init()
+        if self.referat_size > self.max_size and self.abstract_size > self.max_size:
             return answer(False,
-                          f"<br><br>Размер раздела \"Реферат\" равен {referat_size} страницы, должен быть {self.max_size}")
-        if abstract_size > self.max_size:
+                          f"<br><br>Размеры разделов \"Реферат\" и \"Abstract\" превышает максимальный размер")
+        if self.referat_size > self.max_size:
             return answer(False,
-                          f"<br><br>Размер раздела \"Abstract\" равен {abstract_size} страницы, должен быть {self.max_size}")
+                          f"<br><br>Размер раздела \"Реферат\" равен {self.referat_size} страницы, должен быть {self.max_size}")
+        if self.abstract_size > self.max_size:
+            return answer(False,
+                          f"<br><br>Размер раздела \"Abstract\" равен {self.abstract_size} страницы, должен быть {self.max_size}")
         return answer(True,
                       f"<br><br>Размеры разделов \"Реферат\" и \"Abstract\" соответствуют шаблону")
