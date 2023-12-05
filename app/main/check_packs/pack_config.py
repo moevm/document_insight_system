9a27549b376b89955818f983e73ebd5231db6de2
Marina <verika_iz_viro@mail.ru>
2023-12-05 18:56:11 +0300

    full list of check

--- app/main/check_packs/pack_config.py
+++ app/main/check_packs/pack_config.py
@@ -40,7 +40,7 @@ BASE_REPORT_CRITERION = [
     ["report_section_component"],
     ["main_text_check"],
     ['report_template_name'],
-    # ["spelling_check"],
+    ["spelling_check"],
     ["table_share_check"]
 ]
 
