a45c1a105fecfeb5c7f18cffbfcef0aa6b205113
Marina <verika_iz_viro@mail.ru>
2024-02-07 17:53:32 +0300

    tests/requirements.txt added

---
+++ tests/test_version.py
@@ -0,0 +1,14 @@
+import os
+from basic_selenium_test import BasicSeleniumTest
+from selenium.webdriver.common.by import By
+
+class VersionTestSelenium(BasicSeleniumTest):
+
+    def test_version(self):
+        self.authorization()
+        URL = self.getUrl('/version')
+        self.getDriver().get(URL)
+        self.getDriver().implicitly_wait(30)
+        obj = self.getDriver().find_element(By.ID, "version-table")
+        self.assertNotEquals(obj, None)
+    
