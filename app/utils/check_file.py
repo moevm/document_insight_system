a52a9def71dd100b844cf58a99030b57662d36d0
Marina <verika_iz_viro@mail.ru>
2024-02-20 15:32:12 +0300

    MIME for md returned

--- app/utils/check_file.py
+++ app/utils/check_file.py
@@ -1,11 +1,15 @@
 import filetype
 
-def check_file(file, file_extension, allowed_extensions, check_mime):
+def check_file(file, file_extension, allowed_extensions, check_mime=True):
     if not file_extension in allowed_extensions:
         return "not_allowed_extension"
 
-    # Проверяем MIME тип (библиотека автоматически умеет переводить MIME в реальное расширение файла).
-    if check_mime and file_extension != filetype.guess_extension(file):
-        return "mime_type_does_not_match_extension"
+    if check_mime:
+        if file_extension == 'md':
+            if file.mimetype != 'text/plain':
+                return "mime_type_does_not_match_extension"
+        else:
+            if file_extension != filetype.guess_extension(file):
+                return "mime_type_does_not_match_extension"
     
     return "ok"
