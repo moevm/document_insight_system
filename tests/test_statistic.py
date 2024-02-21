0145493a3a16978d9321f8c2b4eeb81f7494f065
Marina <verika_iz_viro@mail.ru>
2024-02-21 17:08:48 +0300

    test_statistic improved

--- tests/test_statistic.py
+++ tests/test_statistic.py
@@ -1,6 +1,7 @@
 import os
 from basic_selenium_test import BasicSeleniumTest
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 class StatisticTestSelenium(BasicSeleniumTest):
 
@@ -9,6 +10,9 @@ class StatisticTestSelenium(BasicSeleniumTest):
         URL = self.getUrl('/check_list')
         self.getDriver().get(URL)
         self.getDriver().implicitly_wait(30)
-        obj = self.getDriver().find_element(By.CLASS_NAME, "fixed-table-container")
-        self.assertNotEquals(obj, None)
-    
+        try:
+            string_in_table = self.getDriver().find_element(By.XPATH, "//table[@id='check-list-table']//tr/td/a")
+            self.assertNotEquals(string_in_table, None)
+        except NoSuchElementException:
+            empty_table = self.getDriver().find_element(By.CLASS_NAME, "no-records-found")
+            self.assertNotEquals(empty_table, None)
