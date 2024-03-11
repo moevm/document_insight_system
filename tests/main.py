a53c77a4396076c3bb1356aa203283877d4c8c15
Marina <verika_iz_viro@mail.ru>
2024-03-11 17:34:46 +0300

    structure and test check changed

--- tests/main.py
+++ tests/main.py
@@ -5,10 +5,9 @@ import argparse
 from basic_selenium_test import BasicSeleniumTest
 from test_statistic import StatisticTestSelenium
 from test_authorization import AuthTestSelenium
-from test_pres_load import PresLoadTestSelenium
 from test_single_card_check import SingleCheckTestSelenium
 from test_version import VersionTestSelenium
-from tests.test_file_load import FileLoadTestSelenium
+from test_file_load import FileLoadTestSelenium
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Run Selenium tests with specified data')
@@ -16,9 +15,9 @@ def parse_arguments():
     parser.add_argument('--login', type=str, required=True, help='insert Username')
     parser.add_argument('--password', type=str, required=True, help='insert Password')
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    parser.add_argument('--pres', type=str, default=os.path.join(script_dir, 'example_of_pres.pptx'), help='your path to press for testing')
-    parser.add_argument('--report', type=str, default=os.path.join(script_dir, 'example_of_report.docx'), help='your path to report in .docx')
-    parser.add_argument('--report_doc', type=str, default=os.path.join(script_dir, 'example_of_report.doc'), help='your path to report in .doc')
+    parser.add_argument('--pres', type=str, default=os.path.join(script_dir, 'tests_data/example_of_pres.pptx'), help='your path to press for testing')
+    parser.add_argument('--report', type=str, default=os.path.join(script_dir, 'tests_data/example_of_report.docx'), help='your path to report in .docx')
+    parser.add_argument('--report_doc', type=str, default=os.path.join(script_dir, 'tests_data/example_of_report.doc'), help='your path to report in .doc')
     
   
     return parser.parse_args()
