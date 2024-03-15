652927503c8beb5144f6060ce867088df1129ce5
TheAnton1 <antontoropygin4@gmail.com>
2024-03-15 13:08:31 +0300

    cosine-similarity-added

--- app/main/checks/report_checks/compare_goal_and_content.py
+++ app/main/checks/report_checks/compare_goal_and_content.py
@@ -1,4 +1,7 @@
-from ..base_check import BaseReportCriterion, answer
+import re
+
+from ..base_check import BaseReportCriterion, answer, morph
+
 
 class CompareGoalAndContentCheck(BaseReportCriterion):
     description = "Проверка соответствия цели, задач и содержания"
@@ -6,6 +9,20 @@ class CompareGoalAndContentCheck(BaseReportCriterion):
 
     def __init__(self, file_info):
         super().__init__(file_info)
+        self.headers = []
+        self.goal = ""
+        self.problems = []
+        self.main_text = []
+
+    def late_init(self):
+        self.headers = self.file.make_headers(self.file_type['report_type'])
 
     def check(self):
-        return answer(True, "Проверка")
+        self.late_init()
+        if self.file.page_counter() < 4:
+            return answer(False, "В отчете недостаточно страниц. Нечего проверять.")
+        result = []
+        for header in self.headers:
+            result.append(header["text"][0:10])
+
+        return answer(True, " ".join(result))
--- playground/nlp/preprocessing.py
+++ playground/nlp/preprocessing.py
@@ -1,4 +1,7 @@
+from collections import Counter, defaultdict
+
 import nltk
+import numpy as np
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
@@ -17,11 +20,7 @@ class NLPProcessor:
         text = text.translate(str.maketrans('', '', string.punctuation))
         tokens = word_tokenize(text)
         tokens = [word for word in tokens if word.lower() not in self.stop_words]
-        return tokens
-
-    def stemming(self, tokens):
-        stemmed_words = [self.stemmer.stem(word) for word in tokens]
-        return stemmed_words
+        return [self.stemmer.stem(token) for token in tokens]
 
     def get_ngrams(self, tokens, n=2):
         result = []
@@ -30,16 +29,52 @@ class NLPProcessor:
             result.extend([' '.join(grams) for grams in n_grams])
         return result
 
+    def get_bag_of_n_gramms(self, corpus):
+        new_corpus = []
+        for item in corpus:
+            for n_gramm in item:
+                new_corpus.append(n_gramm)
+        index_word = {}
+        i = 0
+        for word in new_corpus:
+            if word in index_word.keys():
+                continue
+            index_word[word] = i
+            i += 1
+        return index_word
 
-# # Пример использования
-# nlp_processor = NLPProcessor()
-#
-# text = "Пример текста для стемминга и токенизации. Текст содержит повторяющиеся слова для создания n-грамм."
-# tokens = nlp_processor.preprocessing(text)
-# stemmed_words = nlp_processor.stemming(tokens)
-# n_grams = nlp_processor.get_ngrams(stemmed_words)
-#
-# print("Текст:", text)
-# print("Стеммированные слова:", stemmed_words)
-# print("N-граммы:", n_grams)
-# print("Матрица n-грамм:")
+    def get_vector_by_BOW(self, bag_of_ngramms, doc):
+        count_dict = defaultdict(int)
+        vec = np.zeros(len(bag_of_ngramms))
+        for item in doc:
+            count_dict[item] += 1
+        for key, item in count_dict.items():
+            vec[bag_of_ngramms[key]] = item
+        return vec
+
+    def cosine_similarity(self, vector1, vector2):
+            text1_norm = float(len(vector1)) ** 0.5
+            text2_norm = float(len(vector2)) ** 0.5
+            dot_product = sum(gram_a * gram_b for gram_a, gram_b in zip(vector1, vector2))
+            cosine_sim = dot_product / (text1_norm * text2_norm)
+
+            return cosine_sim
+
+
+# Пример использования
+nlp_processor = NLPProcessor()
+
+text1 = "Реализация алгоритма поиска и отслеживания точек интереса для измерения пульса по видео"
+text2 = "Проанализировать существующие решения для определения видимых областей лица человека и для определения перекрытий объектов, На основе анализа существующих решений сформулировать требования к реализации алгоритма, Реализовать алгоритм поиска и отслеживания точек интереса, Исследовать свойства реализованного алгоритма"
+
+tokens1 = nlp_processor.preprocessing(text1)
+n_grams1 = nlp_processor.get_ngrams(tokens1)
+
+tokens2 = nlp_processor.preprocessing(text2)
+n_grams2 = nlp_processor.get_ngrams(tokens2)
+
+docs = [n_grams1, n_grams2]
+bag_of_ngrams = nlp_processor.get_bag_of_n_gramms(docs)
+vector1 = nlp_processor.get_vector_by_BOW(bag_of_ngrams, n_grams1)
+vector2 = nlp_processor.get_vector_by_BOW(bag_of_ngrams, n_grams2)
+print(f"Согласно косиносному сходству, веторы похожи на {nlp_processor.cosine_similarity(vector1, vector2)}")
