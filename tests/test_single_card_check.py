271e198ddaf67c1e1a276c8d516a39686394c325
Marina <verika_iz_viro@mail.ru>
2024-03-16 16:22:29 +0300

    version.json added

--- tests/test_single_card_check.py
+++ tests/test_single_card_check.py
@@ -9,7 +9,7 @@ class SingleCheckTestSelenium(BasicSeleniumTest):
         self.authorization()
         URL = self.get_url('/check_list')
         self.get_driver().get(URL)
-        self.get_driver().implicitly_wait(240)
+        self.get_driver().implicitly_wait(30)
         single_check = self.get_driver().find_element(By.XPATH, "//table[@id='check-list-table']//tr/td/a")
         id_check_button = single_check.get_attribute("href").split("/")[-1]
         URL = self.get_url(f'/results/{id_check_button}')
