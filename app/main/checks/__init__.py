db018ccaf175d824760f88377773a49c0f23dabb
Marina <verika_iz_virgo@mail.ru>
2024-04-01 19:42:42 +0300

    added: filter, issubclass

--- app/main/checks/__init__.py
+++ app/main/checks/__init__.py
@@ -1,57 +1,20 @@
 import inspect
 from .presentation_checks import *
 from .report_checks import *
+from .base_check import BaseCriterion
 
-members_pres = inspect.getmembers(presentation_checks)
-classes_pres = [member[1] for member in members_pres if inspect.isclass(member[1])]
-members_report = inspect.getmembers(report_checks)
-classes_report = [member[1] for member in members_report if inspect.isclass(member[1])]
+classes_pres = filter(
+    lambda member: inspect.isclass(member[1]) and issubclass(member[1], BaseCriterion) and hasattr(member[1], 'id'),
+    inspect.getmembers(presentation_checks)
+    )
+classes_report = filter(
+    lambda member: inspect.isclass(member[1]) and issubclass(member[1], BaseCriterion) and hasattr(member[1], 'id'),
+    inspect.getmembers(report_checks)
+    )
 
 AVAILABLE_CHECKS = {
-    'pres': {cls.id: cls for cls in classes_pres if hasattr(cls, 'id')},
-    'report': {cls.id: cls for cls in classes_report if hasattr(cls, 'id')}
+    'pres': {cls.id: cls for _, cls in classes_pres},
+    'report': {cls.id: cls for _, cls in classes_report}
 }
 
-# AVAILABLE_CHECKS = {
-#     'pres': {
-#         TemplateNameCheck.id: TemplateNameCheck,
-#         SldNumCheck.id: SldNumCheck,
-#         SldEnumCheck.id: SldEnumCheck,
-#         TitleFormatCheck.id: TitleFormatCheck,
-#         FindDefSld.id: FindDefSld,
-#         SearchKeyWord.id: SearchKeyWord,
-#         FindTasks.id: FindTasks,
-#         SldSimilarity.id: SldSimilarity,
-#         PresRightWordsCheck.id: PresRightWordsCheck,
-#         PresImageShareCheck.id: PresImageShareCheck,
-#         FurtherDev.id: FurtherDev,
-#         PresBannedWordsCheck.id: PresBannedWordsCheck,
-#         PresVerifyGitLinkCheck.id: PresVerifyGitLinkCheck,
-#         PresEmptySlideCheck.id: PresEmptySlideCheck,
-#     },
-#     'report': {
-#         ReportSimpleCheck.id: ReportSimpleCheck,
-#         ReportPageCounter.id: ReportPageCounter,
-#         ReportImageShareCheck.id: ReportImageShareCheck,
-#         LRReportSectionCheck.id: LRReportSectionCheck,
-#         ReportHeadersAtPageTopCheck.id: ReportHeadersAtPageTopCheck,
-#         ReportShortSectionsCheck.id: ReportShortSectionsCheck,
-#         ReportStyleCheck.id: ReportStyleCheck,
-#         ReportBannedWordsCheck.id: ReportBannedWordsCheck,
-#         ReportRightWordsCheck.id: ReportRightWordsCheck,
-#         BannedWordsInLiteratureCheck.id: BannedWordsInLiteratureCheck,
-#         ReferencesToLiteratureCheck.id: ReferencesToLiteratureCheck,
-#         ImageReferences.id: ImageReferences,
-#         TableReferences.id: TableReferences,
-#         ReportFirstPagesCheck.id: ReportFirstPagesCheck,
-#         ReportMainCharacterCheck.id: ReportMainCharacterCheck,
-#         ReportNeededHeadersCheck.id: ReportNeededHeadersCheck,
-#         ReportChapters.id: ReportChapters,
-#         ReportSectionComponent.id: ReportSectionComponent,
-#         ReportMainTextCheck.id: ReportMainTextCheck,
-#         SpellingCheck.id: SpellingCheck
-#     }
-# }
-
-
-
+print(AVAILABLE_CHECKS)
