f537c811385d05f2b2a00e554307fc552b7cd731
Marina <verika_iz_virgo@mail.ru>
2024-04-03 22:27:55 +0300

    page for /criterions

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -1,7 +1,7 @@
-from flask import abort, Blueprint
+from flask import abort, Blueprint, render_template
 from flask_login import current_user
 from functools import wraps
-
+from app.main.checks import CRITERIA_INFO
 
 admin = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
 
@@ -24,7 +24,9 @@ def index():
 @admin.route('/criterions', methods=["GET"])
 @admin_required
 def criterions():
-    return "There will be a list of all system (python classes) criterions"
+    file_type = current_user.file_type['type']
+    table_criteria = CRITERIA_INFO[file_type]
+    return render_template('admin_criterions.html', table_criteria=table_criteria, file_type=file_type)
 
 
 @admin.route('/criterion/<criterion_id>', methods=["GET"])
