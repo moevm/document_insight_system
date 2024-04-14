4bb7fb21862e3d711c5d90b153b4d5db4e0b7cf5
Marina <verika_iz_virgo@mail.ru>
2024-04-14 15:59:14 +0300

    bugs with hidden raw is fixed, showallverdicts button is improved

---
+++ assets/scripts/admin_criterions.js
@@ -0,0 +1,31 @@
+import '../styles/admin_criterions.css';
+
+document.querySelectorAll('.toggle').forEach(item => {
+    item.addEventListener('click', event => {
+        const nextRow = item.parentNode.nextElementSibling;
+        if (nextRow.classList.contains('hidden')) {
+            nextRow.classList.remove('hidden');
+            nextRow.classList.add('visible');
+        } else {
+            nextRow.classList.add('hidden');
+            nextRow.classList.remove('visible');
+        }
+    });
+});
+
+document.getElementById('toggleButton').addEventListener('click', () => {
+    const button = document.getElementById('toggleButton');
+    if (button.innerHTML.trim() === '<i class="bi bi-chevron-double-down"></i>') {
+        button.innerHTML = '<i class="bi bi-chevron-double-up"></i>';
+        document.querySelectorAll('.hidden').forEach(row => {
+            row.classList.remove('hidden');
+            row.classList.add('visible');
+        });
+    } else {
+        button.innerHTML = '<i class="bi bi-chevron-double-down"></i>';
+        document.querySelectorAll('.visible').forEach(row => {
+            row.classList.remove('visible');
+            row.classList.add('hidden');
+        });
+    }
+});
