1086492f8a4bb35e87191483dab93db51d381719
Dmitry Ivanov <darcenrall@gmail.com>
2024-04-29 19:10:49 +0300

    improving parse text for check_similarity

--- app/nlp/similarity_of_texts.py
+++ app/nlp/similarity_of_texts.py
@@ -6,12 +6,18 @@ from nlp.stemming import Stemming
 
 def check_similarity(string1, string2):
     stemming = Stemming()
+    
+    stemming.parse_text(string2, False)
     further_dev = stemming.further_dev()
-    base_conclusions = stemming.get_sentences(string2, False)
+    base_conclusions = stemming.sentences
     ignore = re.compile('[0-9]+[.]?|Заключение|‹#›')
-    clear_conclusions = [ch for ch in base_conclusions if not re.fullmatch(ignore, ch)]
-    recognized_conclusions = "".join(s for s in clear_conclusions if s != further_dev.get('dev_sentence'))
+    conclusions = [ch for ch in base_conclusions if not re.fullmatch(ignore, ch)]
+    cleaned_conclusions = "\n".join(s for s in conclusions if s != further_dev.get('dev_sentence'))
+    
+    tasks = stemming.get_sentences(string1, True)
+    ignore = re.compile('[0-9][.]?|Задачи:|‹#›')  # [:]?
+    cleaned_tasks = "\n".join(task for task in tasks if not re.fullmatch(ignore, task))
 
-    percentage_of_similarity = int(compare_sentences(string1, recognized_conclusions) * 100)
+    percentage_of_similarity = int(compare_sentences(cleaned_tasks, cleaned_conclusions) * 100)
 
-    return percentage_of_similarity, further_dev, recognized_conclusions
+    return percentage_of_similarity, further_dev, conclusions
