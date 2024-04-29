b64de77f4ec6631fbf99bbe2e7901474ee6f39b3
Dmitry Ivanov <darcenrall@gmail.com>
2024-04-29 19:11:37 +0300

    uodate SldSimilarity

--- app/main/checks/presentation_checks/sld_similarity.py
+++ app/main/checks/presentation_checks/sld_similarity.py
@@ -1,6 +1,5 @@
-from nlp.similarity_of_texts import check_similarity
-
 from utils import get_text_from_slides, tasks_conclusions_feedback
+from app.nlp.similarity_of_texts import check_similarity
 from app.nlp.stemming import Stemming
 from ..base_check import BasePresCriterion, answer
 
@@ -19,16 +18,7 @@ class SldSimilarity(BasePresCriterion):
     def check(self):
         goals = get_text_from_slides(self.file, self.goals)
         conclusions = get_text_from_slides(self.file, self.conclusion)
-        if goals == "" or conclusions == "":
-            return answer(False, 'Задач или заключения не существует')
 
-        stemming = Stemming()
-        stemming.parse_text(conclusions, False)
-        try:
-            dev_index = conclusions.index(stemming.further_dev_sentence)
-        except ValueError:
-            dev_index = len(conclusions)
-
-        results = check_similarity(goals, conclusions[:dev_index])
+        results = check_similarity(goals, conclusions)
 
         return answer(results[0] >= self.actual_number, *tasks_conclusions_feedback(results))
