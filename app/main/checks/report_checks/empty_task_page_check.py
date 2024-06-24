bc911b900b28a1a21cc0949c17d4289540d034ef
Marina <verika_iz_virgo@mail.ru>
2024-06-24 21:20:39 +0300

    check is changed (first part)

--- app/main/checks/report_checks/empty_task_page_check.py
+++ app/main/checks/report_checks/empty_task_page_check.py
@@ -1,7 +1,7 @@
 from ..base_check import BaseReportCriterion, answer
 
 PAGE_NAME = "ЗАДАНИЕ НА ВЫПУСКНУЮ КВАЛИФИКАЦИОННУЮ РАБОТУ"
-TASK_PATTERN = "заданиенавыпускнуюквалификационнуюработу"
+
 
 class EmptyTaskPageCheck(BaseReportCriterion):
     label = "Проверка на пустоту страницы с заданием"
@@ -14,12 +14,23 @@ class EmptyTaskPageCheck(BaseReportCriterion):
     def check(self):
         if self.file.page_counter() < 4:
             return answer(False, "В отчете недостаточно страниц. Нечего проверять.")
-        task_page = self.file.pdf_file.text_on_page[2]
-        task_page_string  = task_page.replace('\n', ' ').replace(' ', '').lower()
-        if TASK_PATTERN not in task_page_string:
+        rows_text = self.file.pdf_file.page_rows_text(1)
+        print(rows_text)
+        if 'ЗАДАНИЕ' not in rows_text[0][4]:
             return answer(False, f'Страница "{PAGE_NAME}" не найдена. Убедитесь, что она является второй в документе и не содержит ошибок в заголовке.')
-        task_page_without_head = task_page_string.replace(TASK_PATTERN, '')
-        if len(task_page_without_head) > 1: #not 0 because of page number
-            return answer(True, 'Пройдена!')
-        else:
+        elif len(rows_text) < 4:
             return answer(False, f'Страница "{PAGE_NAME}" не содержит текста.')
+        else:
+            result = {"студент", 'темаработы', 'руководитель'}
+            check_words = {"студент", 'темаработы', 'руководитель'}
+            for k in check_words:
+                for row in rows_text:
+                    row_string = row[4].replace('\n', '').replace(' ', '').replace('_', '').lower()
+                    if k in row_string:
+                        clear_row_string = row_string.replace(k, '')
+                        if len(clear_row_string) > 2:
+                            result.discard(k)
+            if not result:
+                return answer(True, 'Пройдена!')
+            else:
+                return answer(False, f'Некоторые необходимые поля пусты или отсутствуют. Проверьте: {result}')
