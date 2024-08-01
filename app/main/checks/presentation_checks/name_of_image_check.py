267d053d1abbc0d055340983335542f591fc7816
Marina <verika_iz_virgo@mail.ru>
2024-08-01 14:05:22 +0300

    try with size of placeholder

--- app/main/checks/presentation_checks/name_of_image_check.py
+++ app/main/checks/presentation_checks/name_of_image_check.py
@@ -12,16 +12,33 @@ class PresImageNameCheck(BasePresCriterion):
     def check(self):
         wrong_slide = []
         for num, slide in enumerate(self.file.slides, 1):
-            count_image = len(slide.get_images())
-            if count_image > 1:
-                slide_text = slide.get_text()
-                if slide_text.count('Рисунок') != count_image:
-                    wrong_slide.append(num)
-            elif len(slide.get_images()) == 1:
-                # slide_text = slide.get_text()
-                # slide_title = slide.get_titles()
-                pass
+            captions = slide.get_captions()
+            # print(captions, 'cappp', num)
+            for caption in captions:
+                if 'Рисунок' not in caption:
+                    wrong_slide.append(caption)
         if wrong_slide:
             return answer(False, f'{wrong_slide}')
         else:
             return answer(True, 'Пройдена!')
+
+
+
+
+
+
+        #     if count_image > 1:
+        #         slide_text = slide.get_text()
+
+        #         if slide_text.count('Рисунок') != count_image:
+        #             wrong_slide.append(num)
+        #     elif len(slide.get_images()) == 1:
+        #         slide_title = slide.get_title().lower()
+                
+        #         slide_text = slide.get_text().lower().replace(slide_title, '')
+        #         # print(slide_text)
+
+        # if wrong_slide:
+        #     return answer(False, f'{wrong_slide}')
+        # else:
+        #     return answer(True, 'Пройдена!')
--- app/main/presentations/pptx/slide_pptx.py
+++ app/main/presentations/pptx/slide_pptx.py
@@ -9,6 +9,8 @@ class SlidePPTX(SlideBasic):
     def __init__(self, container, w, h, index=-1):
         SlideBasic.__init__(self, container)
         self.dimensions = [w, h]
+        self.size_of_shape = []
+        self.captions = []
         for p in container.placeholders:
             if p.is_placeholder and p.placeholder_format.type == PP_PLACEHOLDER.SLIDE_NUMBER:
                 if p.text == '‹#›':
@@ -26,8 +28,24 @@ class SlidePPTX(SlideBasic):
                 self.images.append(shape)
             if hasattr(shape, "text"):
                 self.text += "\n" + shape.text
+                if self.text.strip():
+                    self.size_of_shape.append((shape.text, shape.top, shape.left, shape.width))
             if shape.has_table:
                 self.table.append(shape)
+
+        if self.images:
+            sorted_size_of_shape = sorted(self.size_of_shape, key=lambda x:x[1])
+            for image in self.images:
+                for top in sorted_size_of_shape:              
+                    if top[1] > (image.top + image.height):
+                        if top[2] > image.left and (top[3]+top[2]) < (image.left+image.width):
+                            caption = top[0]
+                            self.captions.append(caption)
+                            break
+                        if abs(top[2] - image.left) < 400000 and abs((top[3]+top[2]) - (image.left+image.width)) < 400000:
+                            caption = top[0]
+                            self.captions.append(caption)
+                            break
 
     def __str__(self):
         return super().__str__()
