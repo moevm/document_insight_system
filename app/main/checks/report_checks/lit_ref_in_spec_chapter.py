e8203b7033414223b76592b22efada2fcb781864
Marina <verika_iz_virgo@mail.ru>
2024-09-24 21:04:55 +0300

    structure of check is changed (need to improve results string)

--- app/main/checks/report_checks/lit_ref_in_spec_chapter.py
+++ app/main/checks/report_checks/lit_ref_in_spec_chapter.py
@@ -8,26 +8,32 @@ class LitRefInChapter(BaseReportCriterion):
     description = ''
     id = 'references_in_chapter_check'
 
-    def __init__(self, file_info, chapters_for_lit_ref = {
-        # 'введение', "определения, обозначения и сокращения", "заключение"
-        }, min_ref = 1, headers_map = None):
+    def __init__(self, file_info, headers_map = None):
         super().__init__(file_info)
+        self.chapters_for_lit_ref = {}
         self.lit_ref_count = {}
-        self.min_ref = min_ref
         if headers_map:
-            self.chapters_for_lit_ref = StyleCheckSettings.CONFIGS.get(headers_map)[0]["headers_for_lit_count"]
+            self.config = headers_map
         else:
-            self.chapters_for_lit_ref = chapters_for_lit_ref
+            self.config = 'VKR_HEADERS' if (self.file_type['report_type'] == 'VKR') else 'LR_HEADERS'
 
     def late_init(self):
         self.chapters = self.file.make_chapters(self.file_type['report_type'])
+        self.headers_main = self.file.get_main_headers(self.file_type['report_type'])
+        if self.headers_main in StyleCheckSettings.CONFIGS.get(self.config):
+            self.chapters_for_lit_ref = StyleCheckSettings.CONFIGS.get(self.config)[self.headers_main]['chapters_for_lit_ref']
+        else:
+            if 'any_header' in StyleCheckSettings.CONFIGS.get(self.config):
+                self.chapters_for_lit_ref= StyleCheckSettings.CONFIGS.get(self.config)['any_header']['chapters_for_lit_ref']
 
     def check(self):
         if self.file.page_counter() < 4:
             return answer(False, "В отчете недостаточно страниц. Нечего проверять.")
+        self.late_init()
+        if not self.chapters_for_lit_ref:
+            return answer(True, 'Для загруженной работы данная проверка не предусмотрена')
         result = []
         result_str = f'Пройдена!'
-        self.late_init()
         currant_head = ''
         for chapter in self.chapters:
             if chapter['style'] == "heading 2":
@@ -36,12 +42,12 @@ class LitRefInChapter(BaseReportCriterion):
                     self.lit_ref_count[currant_head].append(chapter['number'])
                     if currant_head in self.chapters_for_lit_ref:
                         ref_count = len(self.search_references(self.lit_ref_count[currant_head][0], self.lit_ref_count[currant_head][1]))
-                        if ref_count > self.min_ref:
+                        if ref_count > self.chapters_for_lit_ref[currant_head][1] or ref_count < self.chapters_for_lit_ref[currant_head][0]:
                             result.append(f'"{currant_head[0].upper() + currant_head[1:]}" : {ref_count}')
                 self.lit_ref_count[header] = [chapter['number'],]
                 currant_head = header
         if result:
-            result_str = f'Количество ссылок на источники превышает допустимое ({self.min_ref}) в следующих разделах: <br> {", ".join(res for res in result)}'
+            result_str = f'Количество ссылок на источники не удовлетворяет допустимому в следующих разделах: <br> {", ".join(res for res in result)}'
             return answer(False, result_str)
         else:
             return answer(True, result_str)
