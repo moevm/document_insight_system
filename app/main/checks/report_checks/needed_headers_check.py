23f32c582c221373628aaf4f9e88f67bef4a7c7e
Marina <verika_iz_virgo@mail.ru>
2024-09-28 20:59:53 +0300

    checnged needed_headers_check

--- app/main/checks/report_checks/needed_headers_check.py
+++ app/main/checks/report_checks/needed_headers_check.py
@@ -1,6 +1,6 @@
+
 from .style_check_settings import StyleCheckSettings
 from ..base_check import BaseReportCriterion, answer
-
 
 class ReportNeededHeadersCheck(BaseReportCriterion):
     label = "Проверка наличия обязательных заголовков в отчете"
@@ -13,12 +13,11 @@ class ReportNeededHeadersCheck(BaseReportCriterion):
         self.headers_page = 1
         self.headers = []
         self.main_heading_style = main_heading_style
-        self.patterns = []
         if headers_map:
             self.config = headers_map
         else:
             self.config = 'VKR_HEADERS' if (self.file_type['report_type'] == 'VKR') else 'LR_HEADERS'
-            # self.patterns = StyleCheckSettings.CONFIGS.get(self.config)[0]["headers"]
+    # self.patterns = StyleCheckSettings.CONFIGS.get(self.config)[0]["headers"]
 
     def late_init(self):
         self.headers = self.file.make_chapters(self.file_type['report_type'])
@@ -29,7 +28,32 @@ class ReportNeededHeadersCheck(BaseReportCriterion):
             self.patterns = StyleCheckSettings.CONFIGS.get(self.config)[self.headers_main]['headers']
         else:
             if 'any_header' in StyleCheckSettings.CONFIGS.get(self.config):
+                self.headers_main = 'any_header'
                 self.patterns = StyleCheckSettings.CONFIGS.get(self.config)['any_header']['headers']
+                self.second_lvl_headers = StyleCheckSettings.CONFIGS.get(self.config).get('second_header', {}).get('second_header_check')
+
+    def find_headers_second_lvl(self, header_ind, header_text):
+        result_string_second_lvl = ''
+        final_str = ''
+        start_ind = header_ind+1
+        patterns_for_sec_lvl = []
+        for pattern in self.second_lvl_headers[header_text]:
+            patterns_for_sec_lvl.append({"pattern": pattern, "marker": 0})
+        for header in self.headers[start_ind:]:
+            if header['style'] in StyleCheckSettings.CONFIGS.get(self.config)['second_header']["docx_style"]:
+                for i in range(len(patterns_for_sec_lvl)):
+                    pattern = patterns_for_sec_lvl[i]["pattern"]
+                    if header['text'].lower().find(pattern) >= 0:
+                        patterns_for_sec_lvl[i]["marker"] = 1
+            if header['style'] in StyleCheckSettings.CONFIGS.get(self.config)[self.headers_main]["docx_style"]:
+                break
+
+        for pattern in patterns_for_sec_lvl:
+            if not pattern["marker"]:
+                result_string_second_lvl += '<li>' + pattern["pattern"].upper() + '</li>'
+            if result_string_second_lvl:
+                final_str = f'Подразделы главы "{header_text.upper()}":' + f'<ul>{result_string_second_lvl}</ul>'
+        return final_str
 
     def check(self):
         if self.file.page_counter() < 4:
@@ -38,41 +62,48 @@ class ReportNeededHeadersCheck(BaseReportCriterion):
         if not self.patterns:
             return answer(False, "Не удалось сформировать требуемые заголовки исходя из названия работы. Проверьте наименование работы.")
         result_string = ''
+        result_string_second_lvl = ''
         patterns = []
         for pattern in self.patterns:
             patterns.append({"pattern": pattern, "marker": 0})
         if not len(self.headers):
             return answer(False, "Не найдено ни одного заголовка.<br><br>Проверьте корректность использования стилей.")
+        header_ind = -1
         for header in self.headers:
             header_text = header["text"].lower()
+            header_ind += 1
+            if self.second_lvl_headers:
+                if header_text in self.second_lvl_headers:
+                    result_string_second_lvl = self.find_headers_second_lvl(header_ind, header_text)
             for i in range(len(patterns)):
                 pattern = patterns[i]["pattern"]
-                if header_text.find(pattern.lower()) >= 0:
+                if header_text.find(pattern) >= 0:
                     patterns[i]["marker"] = 1
 
         for pattern in patterns:
             if not pattern["marker"]:
-                result_string += '<li>' + pattern["pattern"] + '</li>'
+                result_string += '<li>' + pattern["pattern"].upper() + '</li>'
 
         if not result_string:
             result_str = f'Все необходимые заголовки обнаружены!'
             result_str += f'<br><br><b>Ниже представлена иерархия обработанных заголовков, ' \
-                        f'сравните с Содержанием {self.format_page_link([self.headers_page])}:</b>'
+            f'сравните с Содержанием {self.format_page_link([self.headers_page])}:</b>'
             result_str += self.chapters_str
             result_str += '<br>Если список не точный, убедитесь, что для каждого заголовка указан верный стиль.'
             return answer(True, result_str)
         else:
             result_str = f'Не найдены следующие обязательные заголовки: <ul>{result_string}</ul>'
+            result_str += result_string_second_lvl
             result_str += '''
-                        Если не найден существующий раздел, попробуйте сделать следующее:
-                        <ul>
-                            <li>Убедитесь в отсутствии опечаток и лишних пробельных символов в названии раздела;</li>
-                            <li>Убедитесь в соответствии стиля заголовка требованиям к отчету по ВКР;</li>
-                            <li>Убедитесь, что заголовок состоит из одного абзаца.</li>
-                        </ul>
-                        '''
+            Если не найден существующий раздел, попробуйте сделать следующее:
+            <ul>
+            <li>Убедитесь в отсутствии опечаток и лишних пробельных символов в названии раздела;</li>
+            <li>Убедитесь в соответствии стиля заголовка требованиям к отчету по ВКР;</li>
+            <li>Убедитесь, что заголовок состоит из одного абзаца.</li>
+            </ul>
+            '''
             result_str += f'<br><br><b>Ниже представлена иерархия обработанных заголовков, ' \
-                        f'сравните с Содержанием {self.format_page_link([self.headers_page])}:</b>'
+            f'сравните с Содержанием {self.format_page_link([self.headers_page])}:</b>'
             result_str += self.chapters_str
             result_str += '<br>Если список не точный, убедитесь, что для каждого заголовка указан верный стиль.'
             return answer(False, result_str)
