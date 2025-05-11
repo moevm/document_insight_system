ccb1d6ecf1c0120006263e0b5edef3d571e5b360
Iskender05 <iskenderomuraliev@mail.ru>
2025-05-11 18:22:55 +0300

    add parse_effective_styles

---
+++ app/main/reports/latex/latex_uploader.py
@@ -0,0 +1,222 @@
+# from ..docx_uploader.core_properties import CoreProperties
+# from ..docx_uploader.inline_shape import InlineShape
+# from ..docx_uploader.paragraph import Paragraph
+# from ..docx_uploader.style import Style
+# from ..docx_uploader.table import Table, Cell
+# from ..pdf_document.pdf_document_manager import PdfDocumentManager
+# from ..document_uploader import DocumentUploader
+from .utils import find_closing_brace
+from .tokenizer import LatexTokenizer
+from .tokenizer import TokenType
+
+
+# Временные заглушки для теста
+class CoreProperties:
+    def __init__(self, title=None, author=None, date=None):
+        self.title = title
+        self.author = author
+        self.date = date
+
+class Paragraph:
+    def __init__(self, _): pass
+
+class Table:
+    def __init__(self, _): pass
+
+class Cell:
+    def __init__(self): pass
+
+
+class LatexUploader(DocumentUploader):
+    def __init__(self):
+        """Инициализирует загрузчик LaTeX-документов."""
+        super().__init__()
+        self.inline_shapes = []
+        self.core_properties = None
+        self.headers = []
+        self.headers_main = ''
+        self.file_path = None
+        self.latex_content = ''
+        self.special_paragraph_indices = {}
+        self.headers_page = 0
+        self.page_count = 0
+
+    def upload(self, file, pdf_filepath=''):
+        """Загружает LaTeX-файл и инициализирует PDF-менеджер."""
+        with open(file, 'r', encoding='utf-8') as f:
+            self.latex_content = f.read()
+        self.file_path = file
+        self.pdf_file = PdfDocumentManager(file, pdf_filepath)
+
+    def extract_preamble(self, latex_content):
+        """Извлекает преамбулу документа."""
+        start = latex_content.find(r'\documentclass')
+        if start == -1:
+            return ''
+        end = latex_content.find(r'\begin{document}', start)
+        return latex_content[start:end] if end != -1 else latex_content[start:]
+
+    def remove_comments(self, text):
+        """Удаляет комментарии из текста."""
+        lines = text.split('\n')
+        return '\n'.join(
+            line[:line.find('%')].rstrip() if '%' in line else line.rstrip()
+            for line in lines
+        )
+
+    def extract_command(self, preamble, command_name):
+        """Извлекает значение LaTeX-команды."""
+        def skip_whitespaces(pos, text):
+            while pos < len(text) and text[pos].isspace():
+                pos += 1
+            return pos
+
+        command_str = f'\\{command_name}'
+        start_idx = preamble.find(command_str)
+        if start_idx == -1:
+            return None
+
+        pos = start_idx + len(command_str)
+        pos = skip_whitespaces(pos, preamble)
+
+        if pos < len(preamble) and preamble[pos] == '[':
+            pos += 1
+            close_pos, _ = find_closing_brace(preamble, pos, '[', ']')
+            if close_pos == -1:
+                return None
+            pos = close_pos + 1
+            pos = skip_whitespaces(pos, preamble)
+
+        if pos >= len(preamble) or preamble[pos] != '{':
+            return None
+
+        pos += 1
+        close_pos, brace_level = find_closing_brace(preamble, pos)
+        if brace_level != 0:
+            return None
+
+        return preamble[pos:close_pos].strip()
+
+    def parse(self):
+        """Основной метод парсинга документа."""
+        raw_preamble = self.extract_preamble(self.latex_content)
+        preamble = self.remove_comments(raw_preamble)
+
+        self.core_properties = CoreProperties(
+            title=self.extract_command(preamble, 'title'),
+            author=self.extract_command(preamble, 'author'),
+            date=self.extract_command(preamble, 'date')
+        )
+
+        tokenizer = LatexTokenizer()
+        self.tokens = tokenizer.tokenize(self.latex_content)
+
+        self._process_tokens()
+
+        self.paragraphs = self.__make_tmp_paragraphs()
+        self.parse_effective_styles()
+        self.tables = self.__make_tmp_tables()
+
+    def _process_tokens(self):
+        """Первичная обработка токенов документа."""
+        self.env_stack = []
+        for token in self.tokens:
+            if token.type == TokenType.COMMAND:
+                self._handle_command_token(token)
+            elif token.type == TokenType.ENVIRONMENT:
+                self._handle_environment_token(token)
+
+    def _handle_command_token(self, token):
+        """Обработка токенов-команд."""
+        pass
+
+    def _handle_environment_token(self, token):
+        """Обработка окружений документа."""
+        pass
+
+    def __make_tmp_paragraphs(self):
+        """Создаёт временные параграфы для тестирования."""
+        return [Paragraph(None) for _ in range(3)]
+
+    def __make_tmp_tables(self):
+        """Создаёт временные таблицы для тестирования."""
+        return [Table([Cell() for _ in range(3)])]
+
+    def parse_effective_styles(self):
+        """Парсит стили текста на основе LaTeX-команд."""
+        styled_paragraphs = []
+        current_paragraph = {"text": "", "runs": []}
+
+        def apply_styles(text, styles):
+            return {
+                "text": text,
+                "style": list(styles)
+            }
+
+        i = 0
+        while i < len(self.tokens):
+            token = self.tokens[i]
+
+            if token.type == TokenType.COMMAND:
+                cmd = token.value
+                if cmd in ["textbf", "textit", "underline", "emph", "texttt"]:
+                    style_map = {
+                        "textbf": "bold",
+                        "textit": "italic",
+                        "underline": "underline",
+                        "emph": "italic",
+                        "texttt": "monospace"
+                    }
+                    if i + 2 < len(self.tokens) and self.tokens[i + 1].type == TokenType.BRACE_OPEN:
+                        j = i + 2
+                        text_inside = ""
+                        brace_level = 1
+                        while j < len(self.tokens) and brace_level > 0:
+                            if self.tokens[j].type == TokenType.BRACE_OPEN:
+                                brace_level += 1
+                            elif self.tokens[j].type == TokenType.BRACE_CLOSE:
+                                brace_level -= 1
+                                if brace_level == 0:
+                                    break
+                            else:
+                                if self.tokens[j].type == TokenType.TEXT:
+                                    text_inside += self.tokens[j].value
+                            j += 1
+
+                        style = style_map[cmd]
+                        current_paragraph["runs"].append(apply_styles(text_inside, [style]))
+                        current_paragraph["text"] += text_inside
+                        i = j
+                    else:
+                        i += 1
+                else:
+                    i += 1
+
+            elif token.type == TokenType.TEXT:
+                current_paragraph["runs"].append(apply_styles(token.value, []))
+                current_paragraph["text"] += token.value
+                i += 1
+
+            elif token.type == TokenType.SPECIAL_CHAR:
+                current_paragraph["runs"].append(apply_styles(" ", []))
+                current_paragraph["text"] += " "
+                i += 1
+            else:
+                i += 1
+
+        if current_paragraph["text"]:
+            styled_paragraphs.append(current_paragraph)
+
+        self.styled_paragraphs = styled_paragraphs
+
+    def upload_from_cli(self, file):
+        """Интерфейс для командной строки."""
+        self.upload(file=file)
+
+    @staticmethod
+    def main(args):
+        """Точка входа CLI."""
+        uploader = LatexUploader()
+        uploader.upload_from_cli(file=args.file)
+        uploader.parse()
+        uploader.print_info()
