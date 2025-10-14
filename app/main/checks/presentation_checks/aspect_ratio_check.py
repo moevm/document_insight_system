3b52a70f79336235963b0c49038dc06776e314ca
baydakov-georgiy <lespend33@gmail.com>
2025-10-14 19:39:01 +0300

    changed correct_ratios param

--- app/main/checks/presentation_checks/aspect_ratio_check.py
+++ app/main/checks/presentation_checks/aspect_ratio_check.py
@@ -2,8 +2,7 @@ import math
 
 from ..base_check import BasePresCriterion, answer
 
-
-class Ratio():
+class Ratio:
     ASPECT_RATIO_EPSILON = 0.01
 
     def __init__(self, width, height):
@@ -31,11 +30,11 @@ class PresAspectRatioCheck(BasePresCriterion):
     description = ""
     id = 'pres_aspect_ratio_check'
 
-    def __init__(self, file_info, correct_ratios=[Ratio(4, 3), Ratio(16, 9)]):
+    def __init__(self, file_info, correct_ratios=["4:3", "16:9"]):
         super().__init__(file_info)
-        self.correct_ratios = correct_ratios        
+        self.correct_ratios = [Ratio(*map(int, x.split(':'))) for x in correct_ratios]
 
-    def _is_correct_ratio(self, aspect_ratio: Ratio):
+    def __is_correct_ratio(self, aspect_ratio: Ratio):
         return any(aspect_ratio == ratio for ratio in self.correct_ratios)
 
     def check(self):
@@ -44,7 +43,7 @@ class PresAspectRatioCheck(BasePresCriterion):
 
         aspect_ratio = Ratio(width, height)
 
-        if self._is_correct_ratio(aspect_ratio):
+        if self.__is_correct_ratio(aspect_ratio):
             return answer(True, f"Соотношение сторон слайдов ({aspect_ratio}) соответствует стандарту.")
 
         correct_ratios_str = ", ".join(map(str, self.correct_ratios))
