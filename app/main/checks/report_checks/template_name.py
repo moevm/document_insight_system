5d82571e0da616072911d1e81bfbf25151fd78fe
Dmitry Ivanov <darcenrall@gmail.com>
2026-02-24 11:03:29 +0300

    fix DEFAULT_TEMPLATE_DESC in ReportTemplateNameCheck

--- app/main/checks/report_checks/template_name.py
+++ app/main/checks/report_checks/template_name.py
@@ -8,9 +8,10 @@ CUR_YEAR = datetime.now().year
 TEMPLATE_TYPES = {
     'GFLab': "Проверка соответствия названия файла шаблону: Отчет_ЛР[1-9]{1}(_[А-Я]{1}[а-яё]*)+, например - 'Отчет_ЛР1_Иванова_Петрова'",
     'CSLab': "Проверка соответствия названия файла шаблону ([номер_студ_билета][фамилия]_ОТЧЕТ_ЛР[номер_ЛР]): [0-9]{6}([А-ЯЁ]+)_ОТЧЕТ_ЛР[1-3]{1}, например - '534111ИВАНОВ_ОТЧЕТ_ЛР1'",
-    'VKR': f"Проверка соответствия названия файла шаблону: {CUR_YEAR}" + r"ВКР[0-9]{6}([А-ЯЁ]+)" + f', например - "{CUR_YEAR}ВКР111111ИВАНОВ"',
-    None:  f"Проверка соответствия названия файла шаблону: {CUR_YEAR}" + r"ВКР[0-9]{6}([А-ЯЁ]+)" + f', например - "{CUR_YEAR}ВКР111111ИВАНОВ"'
+    'VKR': f"Проверка соответствия названия файла шаблону: {CUR_YEAR}" + r"ВКР[0-9]{6}([А-ЯЁ]+)" + f', например - "{CUR_YEAR}ВКР111111ИВАНОВ"'
 }
+DEFAULT_TEMPLATE_DESC = "Проверка соответствия названия файла требуемому шаблону."
+
 
 class ReportTemplateNameCheck(BasePresCriterion):
     label = "Проверка соответствия названия файла шаблону"
@@ -35,6 +36,6 @@ class ReportTemplateNameCheck(BasePresCriterion):
         elif pack == 'ALL':
             return TEMPLATE_TYPES
         for named_pack in TEMPLATE_TYPES:
-            if pack and named_pack in pack:
+            if named_pack in pack:
                 return TEMPLATE_TYPES[named_pack]
-        return TEMPLATE_TYPES[None]
+        return DEFAULT_TEMPLATE_DESC
