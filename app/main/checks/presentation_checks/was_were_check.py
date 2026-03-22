9a758ab96c013688d74aaa3b7894b4e3d2b1936e
baydakov-georgiy <lespend33@gmail.com>
2026-03-23 01:06:24 +0300

    fixed

--- app/main/checks/presentation_checks/was_were_check.py
+++ app/main/checks/presentation_checks/was_were_check.py
@@ -1,27 +1,18 @@
 from ..base_check import BasePresCriterion, answer
-from app.nlp.is_passive_was_were_sentence import CritreriaType, generate_output_text, get_was_were_sentences
+from app.utils.was_were_check import WasWereChecker
+
 
 class PresWasWereCheck(BasePresCriterion):
-    label = 'Проверка на пассивные конструкции, начинающиеся с Был/Была/Было/Были, которые можно убрать без потери смысла'
-    description = ''
-    id = 'pres_was_were_check'
+    label = "Проверка на пассивные конструкции, начинающиеся с Был/Была/Было/Были, которые можно убрать без потери смысла"
+    id = "pres_was_were_check"
 
     def __init__(self, file_info, threshold=3):
         super().__init__(file_info)
         self.threshold = threshold
-    
+        self.checker = WasWereChecker(file_info, threshold)
+
     def check(self):
-        detected_sentences, total_sentences = get_was_were_sentences(self.file, CritreriaType.PRESENTATION)
-        if total_sentences > self.threshold:
-            result_str = generate_output_text(detected_sentences, CritreriaType.PRESENTATION, self.format_page_link)
-            result_score = 0
-        else:
-            if total_sentences != 0:
-                result_str = (
-                    generate_output_text(detected_sentences, CritreriaType.PRESENTATION, self.format_page_link)
-                    + 'Пройдена! (но найдены конструкции, которые можно убрать без потери смысла)'
-                )
-            else:
-                result_str = 'Пройдена!'
-            result_score = 1 
-        return answer(result_score, result_str)
+        message, score = self.checker.get_result_msg_and_score(
+            self.file, self.format_page_link
+        )
+        return answer(score, message)
--- app/main/checks/report_checks/was_were_check.py
+++ app/main/checks/report_checks/was_were_check.py
@@ -1,29 +1,19 @@
 from ..base_check import BaseReportCriterion, answer
-from app.nlp.is_passive_was_were_sentence import CritreriaType, generate_output_text, get_was_were_sentences
+from app.utils.was_were_check import WasWereChecker
+
 
 class ReportWasWereCheck(BaseReportCriterion):
-    label = 'Проверка на пассивные конструкции, начинающиеся с Был/Была/Было/Были, которые можно убрать без потери смысла'
-    description = ''
-    id = 'report_was_were_check'
+    label = "Проверка на пассивные конструкции, начинающиеся с Был/Была/Было/Были, которые можно убрать без потери смысла"
+    _description = "Предложения начинающиеся с Был/Была/Было/Были, которые можно убрать без потери смысла"
+    id = "report_was_were_check"
 
     def __init__(self, file_info, threshold=3):
         super().__init__(file_info)
-        self.threshold = threshold
-    
+        self.checker = WasWereChecker(file_info, threshold)
+
     def check(self):
-        if self.file.page_counter() < 4:
-            return answer(False, 'В отчёте недостаточно страниц. Нечего проверять.')
-        detected, total_sentences = get_was_were_sentences(self.file, CritreriaType.REPORT)
-        if total_sentences > self.threshold:
-            result_str = generate_output_text(detected, CritreriaType.REPORT, self.format_page_link)
-            result_score = 0
-        else:
-            if total_sentences != 0:
-                result_str = (
-                    generate_output_text(detected, CritreriaType.REPORT, self.format_page_link)
-                    + 'Пройдена! (но найдены конструкции, которые можно убрать без потери смысла)'
-                )
-            else:
-                result_str = 'Пройдена!'
-            result_score = 1 
-        return answer(result_score, result_str)
+        message, score = self.checker.get_result_msg_and_score(
+            self.file, self.format_page_link
+        )
+        return answer(score, message)
+
